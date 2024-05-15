06ed2732ff44a09ac6282e50b23e23f5dcd824a7
Sandro-Alessio Gierens <sandro@gierens.de>
2024-05-15 14:48:57 +0200

    feat(lib): add Api.client and use in hello_admin
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,15 +1,36 @@
+use reqwest::blocking::{Client, ClientBuilder};
+use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
+
 pub struct Api {
-    pub url: String,
-    pub token: String,
+    url: String,
+    // token: String,
+    client: Client,
 }
 
 impl Api {
     pub fn new(url: String, token: String) -> Self {
-        Api { url, token }
+        let mut headers = HeaderMap::new();
+        headers
+            .insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+        let value = match HeaderValue::from_str(token.trim()) {
+            Ok(value) => value,
+            Err(e) => {
+                println!("Error: {}", e);
+                HeaderValue::from_static("")
+            }
+        };
+        headers.insert("X-Auth-Token", value);
+        let client = ClientBuilder::new()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+        Api { url, client }
     }
 
     pub fn hello_admin(self) {
-        println!("Hello, admin!");
+        let url = format!("{}/hello/admin", self.url);
+        let response = self.client.get(url).send().unwrap();
+        println!("response: {:?}", response.text());
     }
 
     pub fn hello_user(self) {
