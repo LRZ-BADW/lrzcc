5099bf74a53192cb0f3953166ea221d0f32608e0
Sandro-Alessio Gierens <sandro@gierens.de>
2024-05-15 11:13:10 +0200

    feat(lib): add Api with new, and hello_admin/user functions
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,3 +1,18 @@
-pub fn hello_world() {
-    println!("Hello, world!");
+pub struct Api {
+    pub url: String,
+    pub token: String,
+}
+
+impl Api {
+    pub fn new(url: String, token: String) -> Self {
+        Api { url, token }
+    }
+
+    pub fn hello_admin(self) {
+        println!("Hello, admin!");
+    }
+
+    pub fn hello_user(self) {
+        println!("Hello, user!");
+    }
 }
