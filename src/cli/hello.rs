3fdcc01a088112be16b003d2cd111ceb3d170a3a
Sandro-Alessio Gierens <sandro@gierens.de>
2024-05-24 15:33:56 +0200

    feat(cli): use apply_table_style in hello::admin and hello::user
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/hello.rs
+++ src/cli/hello.rs
@@ -1,10 +1,9 @@
-use crate::common::{Execute, Format};
+use crate::common::{apply_table_style, Execute, Format};
 use clap::Subcommand;
 use lrzcc::hello::Hello;
 use std::borrow::Cow;
 use std::error::Error;
 use tabled::builder::Builder;
-use tabled::settings::Style;
 use tabled::Tabled;
 
 #[derive(Subcommand, Debug)]
@@ -33,20 +32,20 @@ fn admin(api: lrzcc::Api, format: Format) -> Result<(), Box<dyn Error>> {
     let hello = api.hello.admin()?;
     match format {
         Format::Json => println!("{}", serde_json::to_string(&hello)?),
-        _ => {
+        Format::Table(format) => {
             let mut keys = vec![Cow::Owned("key".to_owned())];
             keys.extend(Hello::headers());
             let mut values = vec![Cow::Owned("value".to_owned())];
             values.extend(hello.fields());
             let data = vec![keys, values];
-            let table = Builder::from_iter(data)
+            let mut table = Builder::from_iter(data)
                 .index()
                 .column(0)
                 .transpose()
-                .build()
-                .with(Style::rounded())
-                .to_string();
-            println!("{table}");
+                .build();
+            apply_table_style(&mut table, format);
+            let output = table.to_string();
+            println!("{output}");
         }
     }
     Ok(())
@@ -56,20 +55,20 @@ fn user(api: lrzcc::Api, format: Format) -> Result<(), Box<dyn Error>> {
     let hello = api.hello.user()?;
     match format {
         Format::Json => println!("{}", serde_json::to_string(&hello)?),
-        _ => {
+        Format::Table(format) => {
             let mut keys = vec![Cow::Owned("key".to_owned())];
             keys.extend(Hello::headers());
             let mut values = vec![Cow::Owned("value".to_owned())];
             values.extend(hello.fields());
             let data = vec![keys, values];
-            let table = Builder::from_iter(data)
+            let mut table = Builder::from_iter(data)
                 .index()
                 .column(0)
                 .transpose()
-                .build()
-                .with(Style::rounded())
-                .to_string();
-            println!("{table}");
+                .build();
+            apply_table_style(&mut table, format);
+            let output = table.to_string();
+            println!("{output}");
         }
     }
     Ok(())
