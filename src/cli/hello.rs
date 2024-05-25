038e0efa10654c140f08ec7f06378ef403c3f8e4
Sandro-Alessio Gierens <sandro@gierens.de>
2024-05-25 18:35:53 +0200

    refactor(cli): use print_single_object to simplify hello::admin/user
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/hello.rs
+++ src/cli/hello.rs
@@ -1,10 +1,6 @@
-use crate::common::{apply_table_style, Execute, Format};
+use crate::common::{print_single_object, Execute, Format};
 use clap::Subcommand;
-use lrzcc::hello::Hello;
-use std::borrow::Cow;
 use std::error::Error;
-use tabled::builder::Builder;
-use tabled::Tabled;
 
 #[derive(Subcommand, Debug)]
 pub(crate) enum HelloCommand {
@@ -29,47 +25,9 @@ impl Execute for HelloCommand {
 }
 
 fn admin(api: lrzcc::Api, format: Format) -> Result<(), Box<dyn Error>> {
-    let hello = api.hello.admin()?;
-    match format {
-        Format::Json => println!("{}", serde_json::to_string(&hello)?),
-        Format::Table(format) => {
-            let mut keys = vec![Cow::Owned("key".to_owned())];
-            keys.extend(Hello::headers());
-            let mut values = vec![Cow::Owned("value".to_owned())];
-            values.extend(hello.fields());
-            let data = vec![keys, values];
-            let mut table = Builder::from_iter(data)
-                .index()
-                .column(0)
-                .transpose()
-                .build();
-            apply_table_style(&mut table, format);
-            let output = table.to_string();
-            println!("{output}");
-        }
-    }
-    Ok(())
+    print_single_object(api.hello.admin()?, format)
 }
 
 fn user(api: lrzcc::Api, format: Format) -> Result<(), Box<dyn Error>> {
-    let hello = api.hello.user()?;
-    match format {
-        Format::Json => println!("{}", serde_json::to_string(&hello)?),
-        Format::Table(format) => {
-            let mut keys = vec![Cow::Owned("key".to_owned())];
-            keys.extend(Hello::headers());
-            let mut values = vec![Cow::Owned("value".to_owned())];
-            values.extend(hello.fields());
-            let data = vec![keys, values];
-            let mut table = Builder::from_iter(data)
-                .index()
-                .column(0)
-                .transpose()
-                .build();
-            apply_table_style(&mut table, format);
-            let output = table.to_string();
-            println!("{output}");
-        }
-    }
-    Ok(())
+    print_single_object(api.hello.user()?, format)
 }
