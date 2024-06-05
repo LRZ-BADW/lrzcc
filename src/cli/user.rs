aa3034abb1a262382f6cf5f918d5dc85c8b7cd1b
Sandro-Alessio Gierens <sandro@gierens.de>
2024-06-05 15:12:49 +0200

    feat(cli): add UserListFilter and use in user::list
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/user.rs
+++ src/cli/user.rs
@@ -1,11 +1,24 @@
 use crate::common::{print_object_list, Execute, Format};
-use clap::Subcommand;
+use clap::{Args, Subcommand};
 use std::error::Error;
+
+#[derive(Args, Debug)]
+#[group(multiple = false)]
+pub(crate) struct UserListFilter {
+    #[clap(short, long, help = "Display all users", action)]
+    all: bool,
+
+    #[clap(short, long, help = "Display users of project with given ID")]
+    project: Option<u32>,
+}
 
 #[derive(Subcommand, Debug)]
 pub(crate) enum UserCommand {
     #[clap(about = "List users")]
-    List,
+    List {
+        #[clap(flatten)]
+        filter: UserListFilter,
+    },
 }
 
 impl Execute for UserCommand {
@@ -15,11 +28,21 @@ impl Execute for UserCommand {
         format: Format,
     ) -> Result<(), Box<dyn Error>> {
         match self {
-            UserCommand::List {} => list(api, format),
+            UserCommand::List { filter } => list(api, format, filter),
         }
     }
 }
 
-fn list(api: lrzcc::Api, format: Format) -> Result<(), Box<dyn Error>> {
-    print_object_list(api.user.list()?, format)
+fn list(
+    api: lrzcc::Api,
+    format: Format,
+    filter: &UserListFilter,
+) -> Result<(), Box<dyn Error>> {
+    let mut request = api.user.list();
+    if filter.all {
+        request.all();
+    } else if let Some(project) = filter.project {
+        request.project(project);
+    }
+    print_object_list(request.send()?, format)
 }
