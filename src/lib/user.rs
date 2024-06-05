07e4b7b81f8b50d8567559c918e0871f429d1a20
Sandro-Alessio Gierens <sandro@gierens.de>
2024-06-05 14:28:23 +0200

    feat(lib): add UserListRequest to user and return from UserApi.list
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/user.rs
+++ src/lib/user.rs
@@ -1,6 +1,8 @@
 use crate::common::request;
 use crate::error::ApiError;
+use anyhow::Context;
 use reqwest::blocking::Client;
+use reqwest::Url;
 use reqwest::{Method, StatusCode};
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
@@ -30,6 +32,53 @@ pub struct UserApi {
     pub client: Rc<Client>,
 }
 
+#[derive(Debug)]
+pub struct UserListRequest {
+    url: String,
+    client: Rc<Client>,
+
+    all: bool,
+    project: Option<u32>,
+}
+
+impl UserListRequest {
+    pub fn new(url: &str, client: &Rc<Client>) -> Self {
+        Self {
+            url: url.to_string(),
+            client: Rc::clone(client),
+            all: false,
+            project: None,
+        }
+    }
+
+    fn params(&self) -> Vec<(&str, String)> {
+        let mut params = Vec::new();
+        if self.all {
+            params.push(("all", "1".to_string()));
+        } else if let Some(project) = self.project {
+            params.push(("project", project.to_string()));
+        }
+        params
+    }
+
+    pub fn send(&self) -> Result<Vec<User>, ApiError> {
+        let url = Url::parse_with_params(self.url.as_str(), self.params())
+            .context("Could not parse URL GET parameters.")?;
+        println!("{}", url);
+        request(&self.client, Method::GET, url.as_str(), StatusCode::OK)
+    }
+
+    pub fn all(&mut self) -> &mut Self {
+        self.all = true;
+        self
+    }
+
+    pub fn project(&mut self, project: u32) -> &mut Self {
+        self.project = Some(project);
+        self
+    }
+}
+
 impl UserApi {
     pub fn new(base_url: &str, client: &Rc<Client>) -> UserApi {
         UserApi {
@@ -38,13 +87,7 @@ impl UserApi {
         }
     }
 
-    // TODO we probably need some form of request builder for arguments
-    pub fn list(&self) -> Result<Vec<User>, ApiError> {
-        request(
-            &self.client,
-            Method::GET,
-            format!("{}/", self.url).as_str(),
-            StatusCode::OK,
-        )
+    pub fn list(&self) -> UserListRequest {
+        UserListRequest::new(self.url.as_ref(), &self.client)
     }
 }
