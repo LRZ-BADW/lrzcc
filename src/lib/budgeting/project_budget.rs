2b3eee943382693af00f442bfb8d5924205c262b
Sandro-Alessio Gierens <sandro@gierens.de>
2024-06-17 16:31:46 +0200

    feat(lib): add get function to ProjectBudgetApi
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/budgeting/project_budget.rs
+++ src/lib/budgeting/project_budget.rs
@@ -106,4 +106,10 @@ impl ProjectBudgetApi {
     pub fn list(&self) -> ProjectBudgetListRequest {
         ProjectBudgetListRequest::new(self.url.as_ref(), &self.client)
     }
+
+    pub fn get(&self, id: u32) -> Result<ProjectBudget, ApiError> {
+        // TODO use Url.join
+        let url = format!("{}/{}", self.url, id.to_string());
+        request(&self.client, Method::GET, url.as_str(), StatusCode::OK)
+    }
 }
