d58a8e326868a7501772174a72840f221a8bdf32
Sandro-Alessio Gierens <sandro@gierens.de>
2024-06-17 16:26:49 +0200

    feat(lib): add get function to ServerStateApi
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/accounting/server_state.rs
+++ src/lib/accounting/server_state.rs
@@ -111,4 +111,10 @@ impl ServerStateApi {
     pub fn list(&self) -> ServerStateListRequest {
         ServerStateListRequest::new(self.url.as_ref(), &self.client)
     }
+
+    pub fn get(&self, id: u32) -> Result<ServerState, ApiError> {
+        // TODO use Url.join
+        let url = format!("{}/{}", self.url, id.to_string());
+        request(&self.client, Method::GET, url.as_str(), StatusCode::OK)
+    }
 }
