7f0cc6ad311464de56dfadd00455fd33d7e42123
Sandro-Alessio Gierens <sandro@gierens.de>
2024-06-17 16:19:15 +0200

    feat(lib): add get function to FlavorQuotaApi
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/quota/flavor_quota.rs
+++ src/lib/quota/flavor_quota.rs
@@ -102,4 +102,10 @@ impl FlavorQuotaApi {
     pub fn list(&self) -> FlavorQuotaListRequest {
         FlavorQuotaListRequest::new(self.url.as_ref(), &self.client)
     }
+
+    pub fn get(&self, id: u32) -> Result<FlavorQuota, ApiError> {
+        // TODO use Url.join
+        let url = format!("{}/{}", self.url, id.to_string());
+        request(&self.client, Method::GET, url.as_str(), StatusCode::OK)
+    }
 }
