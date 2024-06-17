a3bf1a70c56cef3fab1dbf16040f2cf284e51c29
Sandro-Alessio Gierens <sandro@gierens.de>
2024-06-17 14:16:42 +0200

    feat(cli): add flavor-price get command
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/pricing.rs
+++ src/cli/pricing.rs
@@ -1,4 +1,4 @@
-use crate::common::{print_object_list, Execute, Format};
+use crate::common::{print_object_list, print_single_object, Execute, Format};
 use clap::Subcommand;
 use std::error::Error;
 
@@ -6,6 +6,9 @@ use std::error::Error;
 pub(crate) enum FlavorPriceCommand {
     #[clap(about = "List users")]
     List,
+
+    #[clap(about = "Show flavor price with given ID")]
+    Get { id: u32 },
 }
 
 impl Execute for FlavorPriceCommand {
@@ -16,6 +19,7 @@ impl Execute for FlavorPriceCommand {
     ) -> Result<(), Box<dyn Error>> {
         match self {
             FlavorPriceCommand::List => list(api, format),
+            FlavorPriceCommand::Get { id } => get(api, format, id),
         }
     }
 }
@@ -23,4 +27,12 @@ impl Execute for FlavorPriceCommand {
 fn list(api: lrzcc::Api, format: Format) -> Result<(), Box<dyn Error>> {
     let request = api.flavor_price.list();
     print_object_list(request.send()?, format)
+}
+
+fn get(
+    api: lrzcc::Api,
+    format: Format,
+    id: &u32,
+) -> Result<(), Box<dyn Error>> {
+    print_single_object(api.flavor_price.get(*id)?, format)
 }
