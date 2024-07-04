a0c8d4c5ac85dea81f5783f20575f469358d8e5c
Sandro-Alessio Gierens <sandro@gierens.de>
2024-07-04 17:45:40 +0200

    feat(cli): add flavor-group modify command
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/resources/flavor_group.rs
+++ src/cli/resources/flavor_group.rs
@@ -26,6 +26,22 @@ pub(crate) enum FlavorGroupCommand {
         name: String,
     },
 
+    #[clap(about = "Modify a flavor group")]
+    Modify {
+        #[clap(help = "ID of the flavor group")]
+        id: u32,
+
+        #[clap(long, short, help = "Name of the flavor group")]
+        name: Option<String>,
+
+        #[clap(
+            long,
+            short,
+            help = "ID of the project this flavor group belongs to"
+        )]
+        project: Option<u32>,
+    },
+
     #[clap(about = "Delete flavor group with given ID")]
     Delete { id: u32 },
 }
@@ -41,6 +57,9 @@ impl Execute for FlavorGroupCommand {
             List { filter } => list(api, format, filter),
             Get { id } => get(api, format, id),
             Create { name } => create(api, format, name.to_owned()),
+            Modify { id, name, project } => {
+                modify(api, format, *id, name.to_owned(), project.to_owned())
+            }
             Delete { id } => delete(api, id),
         }
     }
@@ -72,6 +91,23 @@ fn create(
     name: String,
 ) -> Result<(), Box<dyn Error>> {
     print_single_object(api.flavor_group.create(name).send()?, format)
+}
+
+fn modify(
+    api: lrzcc::Api,
+    format: Format,
+    id: u32,
+    name: Option<String>,
+    project: Option<u32>,
+) -> Result<(), Box<dyn Error>> {
+    let mut request = api.flavor_group.modify(id);
+    if let Some(name) = name {
+        request.name(name);
+    }
+    if let Some(project) = project {
+        request.project(project);
+    }
+    print_single_object(request.send()?, format)
 }
 
 fn delete(api: lrzcc::Api, id: &u32) -> Result<(), Box<dyn Error>> {
