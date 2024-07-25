233a29c514857c9190ff379b3754f5f7b3ef073d
Sandro-Alessio Gierens <sandro@gierens.de>
2024-07-25 17:01:45 +0200

    feat(cli): use flavor::find_id on flavor get, modify and delete commands
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/resources/flavor.rs
+++ src/cli/resources/flavor.rs
@@ -2,6 +2,7 @@ use crate::common::{
     ask_for_confirmation, print_object_list, print_single_object, Execute,
     Format,
 };
+use anyhow::{anyhow, Context};
 use clap::{Args, Subcommand};
 use std::error::Error;
 
@@ -24,14 +25,15 @@ pub(crate) enum FlavorCommand {
         filter: FlavorListFilter,
     },
 
-    #[clap(about = "Show flavor with given ID")]
-    Get { id: u32 },
+    #[clap(about = "Show flavor with given name, ID or OpenStack UUIDv4")]
+    Get { name_or_id: String },
 
     #[clap(about = "Create a new flavor")]
     Create {
         #[clap(help = "Name of the flavor")]
         name: String,
 
+        // TODO verify that this is a UUIDv4
         #[clap(help = "Openstack UUIDv4 of the flavor")]
         openstack_id: String,
 
@@ -44,8 +46,8 @@ pub(crate) enum FlavorCommand {
 
     #[clap(about = "Modify a flavor")]
     Modify {
-        #[clap(help = "ID of the flavor")]
-        id: u32,
+        #[clap(help = "Name, ID or OpenStack UUIDv4 of the flavor")]
+        name_or_id: String,
 
         #[clap(long, short, help = "Name of the flavor")]
         name: Option<String>,
@@ -66,8 +68,8 @@ pub(crate) enum FlavorCommand {
         no_group: bool,
     },
 
-    #[clap(about = "Delete flavor with given ID")]
-    Delete { id: u32 },
+    #[clap(about = "Delete flavor with given name, ID or OpenStack UUIDv4")]
+    Delete { name_or_id: String },
 }
 pub(crate) use FlavorCommand::*;
 
@@ -79,7 +81,7 @@ impl Execute for FlavorCommand {
     ) -> Result<(), Box<dyn Error>> {
         match self {
             List { filter } => list(api, format, filter),
-            Get { id } => get(api, format, id),
+            Get { name_or_id } => get(api, format, name_or_id),
             Create {
                 name,
                 openstack_id,
@@ -94,7 +96,7 @@ impl Execute for FlavorCommand {
                 *weight,
             ),
             Modify {
-                id,
+                name_or_id,
                 name,
                 openstack_id,
                 group,
@@ -102,13 +104,13 @@ impl Execute for FlavorCommand {
             } => modify(
                 api,
                 format,
-                *id,
+                name_or_id,
                 name.clone(),
                 openstack_id.clone(),
                 *group,
                 *no_group,
             ),
-            Delete { id } => delete(api, id),
+            Delete { name_or_id } => delete(api, name_or_id),
         }
     }
 }
@@ -130,9 +132,10 @@ fn list(
 fn get(
     api: lrzcc::Api,
     format: Format,
-    id: &u32,
+    name_or_id: &str,
 ) -> Result<(), Box<dyn Error>> {
-    print_single_object(api.flavor.get(*id)?, format)
+    let id = find_id(&api, name_or_id)?;
+    print_single_object(api.flavor.get(id)?, format)
 }
 
 fn create(
@@ -156,12 +159,13 @@ fn create(
 fn modify(
     api: lrzcc::Api,
     format: Format,
-    id: u32,
+    name_or_id: &str,
     name: Option<String>,
     openstack_id: Option<String>,
     group: Option<u32>,
     no_group: bool,
 ) -> Result<(), Box<dyn Error>> {
+    let id = find_id(&api, name_or_id)?;
     let mut request = api.flavor.modify(id);
     if let Some(name) = name {
         request.name(name);
@@ -177,9 +181,11 @@ fn modify(
     print_single_object(request.send()?, format)
 }
 
-fn delete(api: lrzcc::Api, id: &u32) -> Result<(), Box<dyn Error>> {
+// TODO replace all command functions errors by anyhow::Error
+fn delete(api: lrzcc::Api, name_or_id: &str) -> Result<(), Box<dyn Error>> {
+    let id = find_id(&api, name_or_id)?;
     ask_for_confirmation()?;
-    Ok(api.flavor.delete(*id)?)
+    Ok(api.flavor.delete(id)?)
 }
 
 pub(crate) fn find_id(
