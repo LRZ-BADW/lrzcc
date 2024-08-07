59573ee60351cc6e83cfc45f9cd7181f3c44b16a
Sandro-Alessio Gierens <sandro@gierens.de>
2024-08-07 17:20:43 +0200

    feat(lib): add server/user/project/all filter to ServerCostRequest
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/accounting/server_cost.rs
+++ src/lib/accounting/server_cost.rs
@@ -20,6 +20,10 @@ pub struct ServerCostRequest {
 
     begin: Option<DateTime<FixedOffset>>,
     end: Option<DateTime<FixedOffset>>,
+    server: Option<String>,
+    user: Option<u32>,
+    project: Option<u32>,
+    all: bool,
 }
 
 impl ServerCostRequest {
@@ -30,6 +34,10 @@ impl ServerCostRequest {
 
             begin: None,
             end: None,
+            server: None,
+            user: None,
+            project: None,
+            all: false,
         }
     }
 
@@ -37,8 +45,18 @@ impl ServerCostRequest {
         let mut params = Vec::new();
         if let Some(begin) = self.begin {
             params.push(("begin", begin.to_rfc3339().to_string()));
-        } else if let Some(end) = self.end {
+        }
+        if let Some(end) = self.end {
             params.push(("end", end.to_rfc3339().to_string()));
+        }
+        if let Some(server) = &self.server {
+            params.push(("server", server.to_string()));
+        } else if let Some(user) = self.user {
+            params.push(("user", user.to_string()));
+        } else if let Some(project) = self.project {
+            params.push(("project", project.to_string()));
+        } else if self.all {
+            params.push(("all", "1".to_string()));
         }
         params
     }
@@ -50,6 +68,26 @@ impl ServerCostRequest {
 
     pub fn end(&mut self, end: DateTime<FixedOffset>) -> &mut Self {
         self.end = Some(end);
+        self
+    }
+
+    pub fn server(&mut self, server: &str) -> &mut Self {
+        self.server = Some(server.to_string());
+        self
+    }
+
+    pub fn user(&mut self, user: u32) -> &mut Self {
+        self.user = Some(user);
+        self
+    }
+
+    pub fn project(&mut self, project: u32) -> &mut Self {
+        self.project = Some(project);
+        self
+    }
+
+    pub fn all(&mut self) -> &mut Self {
+        self.all = true;
         self
     }
 
