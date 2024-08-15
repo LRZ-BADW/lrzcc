bf752926daa87ddfca504233f787ed487fd926c5
Sandro-Alessio Gierens <sandro@gierens.de>
2024-08-15 12:54:10 +0200

    refactor(lib): revise error_chain_fmt output
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -18,10 +18,10 @@ fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
-    writeln!(f, "{}\n", e)?;
+    writeln!(f, "{}", e)?;
     let mut current = e.source();
     while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{}", cause)?;
+        writeln!(f, "caused by: {}", cause)?;
         current = cause.source();
     }
     Ok(())
