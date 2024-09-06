4568a34592afd4937910d60dc5fe1bfece5a307e
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-07 00:10:23 +0200

    feat(api/authentication): implement keystone authentication
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/authentication.rs
+++ api/src/authentication.rs
@@ -1,20 +1,35 @@
+use crate::openstack::OpenStack;
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::error::InternalError;
 use actix_web::middleware::Next;
-use actix_web::HttpResponse;
+use actix_web::web::Data;
+use actix_web::{HttpMessage, HttpResponse};
 
-pub async fn reject_anonymous_users(
+pub async fn require_valid_token(
     req: ServiceRequest,
     next: Next<impl MessageBody>,
 ) -> Result<ServiceResponse<impl MessageBody>, actix_web::Error> {
-    // TODO implement actual authentication logic
-    let succeed = false;
-    if succeed {
-        Ok(next.call(req).await?)
-    } else {
+    let Some(token) = req.headers().get("X-Auth-Token") else {
         let response = HttpResponse::Unauthorized().finish();
-        let e = anyhow::anyhow!("The user has not logged in");
-        Err(InternalError::from_response(e, response).into())
-    }
+        let e = anyhow::anyhow!("No token in request header");
+        return Err(InternalError::from_response(e, response).into());
+    };
+    let Ok(token) = token.to_str() else {
+        let response = HttpResponse::BadRequest().finish();
+        let e = anyhow::anyhow!("Token is not a valid string");
+        return Err(InternalError::from_response(e, response).into());
+    };
+    let Some(openstack) = req.app_data::<Data<OpenStack>>() else {
+        let response = HttpResponse::InternalServerError().finish();
+        let e = anyhow::anyhow!("No OpenStack client in application state");
+        return Err(InternalError::from_response(e, response).into());
+    };
+    let Ok(project) = openstack.validate_user_token(token).await else {
+        let response = HttpResponse::Unauthorized().finish();
+        let e = anyhow::anyhow!("Failed to validate user token");
+        return Err(InternalError::from_response(e, response).into());
+    };
+    req.extensions_mut().insert(project);
+    next.call(req).await
 }
