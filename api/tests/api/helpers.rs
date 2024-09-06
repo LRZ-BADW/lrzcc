68c9e53bcf997229b6156fa6386b980c7e3a31a1
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-06 11:44:22 +0200

    test(helpers): remove unneeded function assert_is_redirect_to
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/tests/api/helpers.rs
+++ api/tests/api/helpers.rs
@@ -89,8 +89,3 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
 
     connection_pool
 }
-
-pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
-    assert_eq!(response.status().as_u16(), 303);
-    assert_eq!(response.headers().get("Location").unwrap(), location);
-}
