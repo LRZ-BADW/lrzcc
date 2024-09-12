8017256717ed0dce52191749c7aeb43ce8f94f2b
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-12 09:14:38 +0200

    fix(wire/common): make display_option public
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/common.rs
+++ wire/src/common.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
 #[allow(dead_code)]
-pub(crate) fn display_option<T: Display>(option: &Option<T>) -> String {
+pub fn display_option<T: Display>(option: &Option<T>) -> String {
     match option {
         Some(value) => value.to_string(),
         None => "".to_string(),
