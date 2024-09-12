571b28097c7355738eeed321de69efb71eb0ee34
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-12 08:38:52 +0200

    feat(wire/error): derive Clone, Debug, Tabled for ErrorResponse
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/error.rs
+++ wire/src/error.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
+use tabled::Tabled;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Tabled)]
 pub struct ErrorResponse {
     pub detail: String,
 }
