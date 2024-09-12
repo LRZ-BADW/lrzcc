6d1497fdef39fba2d93e21019275357202e5a78b
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-12 14:55:59 +0200

    feat(api/hello): add simplified message in hello endpoint and remove unit test
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -1,18 +1,15 @@
+use crate::openstack::ProjectMinimal;
+use actix_web::web::ReqData;
 use actix_web::HttpResponse;
+use lrzcc_wire::hello::Hello;
 
 #[tracing::instrument(name = "hello")]
-pub async fn hello() -> Result<HttpResponse, actix_web::Error> {
-    // TODO implement
-    Ok(HttpResponse::Ok().finish())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn hello_works() {
-        let response = hello().await.unwrap();
-        assert!(response.status().is_success())
-    }
+pub async fn hello(
+    project: ReqData<ProjectMinimal>,
+) -> Result<HttpResponse, actix_web::Error> {
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .json(Hello {
+            message: format!("Hello, user {}!", project.name),
+        }))
 }
