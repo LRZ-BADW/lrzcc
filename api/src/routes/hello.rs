9f52ccdcb59bfc816810771aa3a1f1cf1f7183da
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-13 08:22:37 +0200

    feat(api/hello): rename project to os_project
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -1,15 +1,15 @@
-use crate::openstack::ProjectMinimal;
+use crate::openstack::ProjectMinimal as OpenstackProjectMinimal;
 use actix_web::web::ReqData;
 use actix_web::HttpResponse;
 use lrzcc_wire::hello::Hello;
 
 #[tracing::instrument(name = "hello")]
 pub async fn hello(
-    project: ReqData<ProjectMinimal>,
+    os_project: ReqData<OpenstackProjectMinimal>,
 ) -> Result<HttpResponse, actix_web::Error> {
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(Hello {
-            message: format!("Hello, user {}!", project.name),
+            message: format!("Hello, user {}!", os_project.name),
         }))
 }
