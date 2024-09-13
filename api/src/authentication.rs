f1dd76d199947784fe3bead87894286d6725d397
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-13 09:53:29 +0200

    feat(api/auth): start extract_user_and_project middleware function
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/authentication.rs
+++ api/src/authentication.rs
@@ -1,10 +1,11 @@
-use crate::openstack::OpenStack;
+use crate::openstack::{OpenStack, ProjectMinimal as OpenstackProjectMinimal};
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::error::InternalError;
 use actix_web::middleware::Next;
 use actix_web::web::Data;
 use actix_web::{HttpMessage, HttpResponse};
+use sqlx::MySqlPool;
 
 pub async fn require_valid_token(
     req: ServiceRequest,
@@ -31,5 +32,25 @@ pub async fn require_valid_token(
         return Err(InternalError::from_response(e, response).into());
     };
     req.extensions_mut().insert(os_project);
+    next.call(req).await
+}
+
+pub async fn extract_user_and_project(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, actix_web::Error> {
+    let Some(os_project) = req.extensions().get::<OpenstackProjectMinimal>()
+    else {
+        let response = HttpResponse::InternalServerError().finish();
+        let e = anyhow::anyhow!("No OpenStack project in request extensions");
+        return Err(InternalError::from_response(e, response).into());
+    };
+    let Some(db_pool) = req.app_data::<Data<MySqlPool>>() else {
+        let response = HttpResponse::InternalServerError().finish();
+        let e =
+            anyhow::anyhow!("No database connection pool in application state");
+
+        return Err(InternalError::from_response(e, response).into());
+    };
     next.call(req).await
 }
