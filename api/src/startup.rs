f492b10c823729062dd5130c16fea81fb19a0632
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-14 15:35:43 +0200

    feat(api/startup): revise routes for hello_user/admin
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/startup.rs
+++ api/src/startup.rs
@@ -1,7 +1,7 @@
 use crate::authentication::{extract_user_and_project, require_valid_token};
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::openstack::OpenStack;
-use crate::routes::{health_check, hello};
+use crate::routes::{health_check, hello_admin, hello_user};
 use actix_web::{
     dev::Server, middleware::from_fn, web, web::Data, App, HttpServer,
 };
@@ -69,7 +69,8 @@ async fn run(
                     .wrap(from_fn(extract_user_and_project))
                     .wrap(from_fn(require_valid_token))
                     .route("/secured_health_check", web::get().to(health_check))
-                    .route("/hello", web::get().to(hello)),
+                    .route("/hello", web::get().to(hello_user))
+                    .route("/hello/admin", web::get().to(hello_admin)),
             )
         // )
     })
