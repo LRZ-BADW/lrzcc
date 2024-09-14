cc4633b56e5a22fbd14a878cfc53187e77319aab
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-14 15:26:40 +0200

    feat(api/hello): revise hello handler to extract user and project
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -1,15 +1,16 @@
-use crate::openstack::ProjectMinimal as OpenstackProjectMinimal;
 use actix_web::web::ReqData;
 use actix_web::HttpResponse;
 use lrzcc_wire::hello::Hello;
+use lrzcc_wire::user::{Project, User};
 
 #[tracing::instrument(name = "hello")]
 pub async fn hello(
-    os_project: ReqData<OpenstackProjectMinimal>,
+    user: ReqData<User>,
+    project: ReqData<Project>,
 ) -> Result<HttpResponse, actix_web::Error> {
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(Hello {
-            message: format!("Hello, user {}!", os_project.name),
+            message: format!("Hello, user {}!", user.name),
         }))
 }
