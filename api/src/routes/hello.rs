3f1392a1e6a1ad626cf21a0fe0dd18308d697308
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-14 15:35:02 +0200

    feat(api/hello): replace hello by hello_user and implement
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -3,14 +3,17 @@ use actix_web::HttpResponse;
 use lrzcc_wire::hello::Hello;
 use lrzcc_wire::user::{Project, User};
 
-#[tracing::instrument(name = "hello")]
-pub async fn hello(
+#[tracing::instrument(name = "hello_user")]
+pub async fn hello_user(
     user: ReqData<User>,
     project: ReqData<Project>,
 ) -> Result<HttpResponse, actix_web::Error> {
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(Hello {
-            message: format!("Hello, user {}!", user.name),
+            message: format!(
+                "Hello, {} from project {} with user class {}",
+                user.name, project.name, project.user_class
+            ),
         }))
 }
