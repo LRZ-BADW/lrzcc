02ff7006e973cc206309847a9defbd3b068eca6a
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-17 09:52:54 +0200

    feat(api/hello): add hello_scope function
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -1,7 +1,18 @@
+use crate::authentication::require_admin_user;
+use actix_web::middleware::from_fn;
 use actix_web::web::ReqData;
-use actix_web::HttpResponse;
+use actix_web::web::{get, scope};
+use actix_web::{HttpResponse, Scope};
 use lrzcc_wire::hello::Hello;
 use lrzcc_wire::user::{Project, User};
+
+pub fn hello_scope() -> Scope {
+    scope("/hello").route("", get().to(hello_user)).service(
+        scope("")
+            .wrap(from_fn(require_admin_user))
+            .route("/admin", get().to(hello_admin)),
+    )
+}
 
 #[tracing::instrument(name = "hello_user")]
 pub async fn hello_user(
