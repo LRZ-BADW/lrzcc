efdc5800b83ae6262fb8ccd3ae4574ed18528b7f
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-17 11:59:36 +0200

    test(test/user/project): add e2e_lib_project_get_for_own_project_of_admin
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project.rs
+++ test/tests/user/project.rs
@@ -81,3 +81,40 @@ async fn e2e_lib_project_create_returns_not_found() {
     .await
     .unwrap();
 }
+
+// TODO this is just temporary, a general not found test is better for this
+#[tokio::test]
+async fn e2e_lib_project_get_for_own_project_of_admin() {
+    // arrange
+    let server = spawn_app().await;
+    let (user, project, token) = server
+        .setup_test_user_and_project(true)
+        .await
+        .expect("Failed to setup test user and project.");
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let project_detailed = client.project.get(project.id).unwrap();
+
+        // assert
+        assert_eq!(project.id, project_detailed.id);
+        assert_eq!(project.name, project_detailed.name);
+        assert_eq!(project.openstack_id, project_detailed.openstack_id);
+        assert_eq!(project.user_class, project_detailed.user_class);
+    })
+    .await
+    .unwrap();
+}
