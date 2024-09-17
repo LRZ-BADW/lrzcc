2ced8e960ac647199a9da2613cfa08f96a8a09a2
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-17 11:58:24 +0200

    feat(api/user/project): add rudimentary project_get handler
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project.rs
+++ api/src/routes/user/project.rs
@@ -1,16 +1,78 @@
 use crate::authentication::require_admin_user;
-use crate::error::not_found;
+use crate::error::{not_found, not_found_error};
 use actix_web::middleware::from_fn;
-use actix_web::web::{delete, get, patch, post, scope};
-use actix_web::Scope;
+use actix_web::web::{delete, get, patch, post, scope, Data, Path, ReqData};
+use actix_web::{HttpResponse, Scope};
+use lrzcc_wire::user::{Project, ProjectDetailed, User};
+use serde::Deserialize;
+use sqlx::MySqlPool;
 
 pub fn projects_scope() -> Scope {
     scope("/projects").service(
         scope("")
             .wrap(from_fn(require_admin_user))
             .route("", post().to(not_found))
-            .route("", get().to(not_found))
+            .route("/{project_id}", get().to(project_get))
+            // TODO: what about push?
             .route("", patch().to(not_found))
             .route("", delete().to(not_found)),
     )
+}
+
+#[derive(Deserialize, Debug)]
+struct ProjectGetParams {
+    // TODO: why is this necessary, when this is clearly read in query_as
+    #[allow(unused)]
+    project_id: u32,
+}
+
+// TODO proper query set and permissions
+#[tracing::instrument(name = "project_get")]
+async fn project_get(
+    user: ReqData<User>,
+    project: ReqData<Project>,
+    db_pool: Data<MySqlPool>,
+    params: Path<ProjectGetParams>,
+) -> Result<HttpResponse, actix_web::Error> {
+    struct Row {
+        id: i32,
+        name: String,
+        openstack_id: String,
+        user_class: u32,
+    }
+
+    let Ok(row) = sqlx::query_as!(
+        Row,
+        r#"
+        SELECT
+            id,
+            name,
+            openstack_id,
+            user_class
+        FROM user_project AS project
+        WHERE
+            project.id = ?
+        "#,
+        params.project_id,
+    )
+    .fetch_one(db_pool.get_ref())
+    .await
+    else {
+        // TODO apply context and map_err
+        return Err(not_found_error("Project with given ID not found"));
+    };
+
+    let project = ProjectDetailed {
+        id: row.id as u32,
+        name: row.name,
+        openstack_id: row.openstack_id,
+        user_class: row.user_class,
+        // TODO retrieve actual values
+        users: vec![],
+        flavor_groups: vec![],
+    };
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .json(project))
 }
