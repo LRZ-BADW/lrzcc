d500f047de189541c549c76783266025393e5b2a
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-17 14:13:17 +0200

    feat(api/user/project): add project_delete handler
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project.rs
+++ api/src/routes/user/project.rs
@@ -12,15 +12,16 @@ pub fn projects_scope() -> Scope {
         scope("")
             .wrap(from_fn(require_admin_user))
             .route("", post().to(not_found))
+            .route("", get().to(not_found))
             .route("/{project_id}", get().to(project_get))
-            // TODO: what about push?
+            // TODO: what about PUT?
             .route("", patch().to(not_found))
-            .route("", delete().to(not_found)),
+            .route("/{project_id}/", delete().to(project_delete)),
     )
 }
 
 #[derive(Deserialize, Debug)]
-struct ProjectGetParams {
+struct ProjectIdParam {
     // TODO: why is this necessary, when this is clearly read in query_as
     #[allow(unused)]
     project_id: u32,
@@ -32,7 +33,7 @@ async fn project_get(
     user: ReqData<User>,
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
-    params: Path<ProjectGetParams>,
+    params: Path<ProjectIdParam>,
 ) -> Result<HttpResponse, actix_web::Error> {
     struct Row {
         id: i32,
@@ -58,6 +59,7 @@ async fn project_get(
     .fetch_one(db_pool.get_ref())
     .await
     else {
+        // TODO there might be other errors as well
         // TODO apply context and map_err
         return Err(not_found_error("Project with given ID not found"));
     };
@@ -75,4 +77,27 @@ async fn project_get(
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(project))
+}
+
+#[tracing::instrument(name = "project_delete")]
+async fn project_delete(
+    user: ReqData<User>,
+    project: ReqData<Project>,
+    db_pool: Data<MySqlPool>,
+    params: Path<ProjectIdParam>,
+) -> Result<HttpResponse, actix_web::Error> {
+    if sqlx::query!(
+        r#"DELETE FROM user_project WHERE id = ?"#,
+        params.project_id,
+    )
+    .execute(db_pool.get_ref())
+    .await
+    .is_err()
+    {
+        // TODO there might be other errors as well
+        // TODO apply context and map_err
+        return Err(not_found_error("Project with given ID not found"));
+    };
+
+    Ok(HttpResponse::NoContent().finish())
 }
