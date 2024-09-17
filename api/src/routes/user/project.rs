fb0a579a380b72517922057b80027c398f346eaf
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-17 22:26:40 +0200

    feat(api/user): add rudimentary project_create handler
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project.rs
+++ api/src/routes/user/project.rs
@@ -1,17 +1,25 @@
 use crate::authentication::require_admin_user;
-use crate::error::{internal_server_error, not_found, not_found_error};
+use crate::error::{
+    bad_request_error, internal_server_error, not_found, not_found_error,
+};
 use actix_web::middleware::from_fn;
-use actix_web::web::{delete, get, patch, post, scope, Data, Path, ReqData};
+use actix_web::web::{
+    delete, get, patch, post, scope, Data, Json, Path, ReqData,
+};
 use actix_web::{HttpResponse, Scope};
-use lrzcc_wire::user::{Project, ProjectDetailed, User};
+use lrzcc_wire::user::{
+    Project, ProjectCreateData, ProjectCreated, ProjectDetailed, User,
+};
 use serde::Deserialize;
-use sqlx::MySqlPool;
+use sqlx::{Executor, MySqlPool};
+
+// TODO use anyhow and thiserror
 
 pub fn projects_scope() -> Scope {
     scope("/projects").service(
         scope("")
             .wrap(from_fn(require_admin_user))
-            .route("", post().to(not_found))
+            .route("", post().to(project_create))
             .route("", get().to(project_list))
             .route("/{project_id}", get().to(project_get))
             // TODO: what about PUT?
@@ -32,6 +40,46 @@ struct ProjectRow {
     name: String,
     openstack_id: String,
     user_class: u32,
+}
+
+#[tracing::instrument(name = "project_create")]
+async fn project_create(
+    user: ReqData<User>,
+    project: ReqData<Project>,
+    db_pool: Data<MySqlPool>,
+    data: Json<ProjectCreateData>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let user_class = data.user_class.unwrap_or(1);
+    // TODO: MariaDB 10.5 introduced INSERT ... RETURNING
+    let query = sqlx::query!(
+        r#"
+        INSERT INTO user_project (name, openstack_id, user_class)
+        VALUES (?, ?, ?)
+        "#,
+        data.name,
+        data.openstack_id,
+        data.user_class
+    );
+    let Ok(result) = db_pool.execute(query).await else {
+        // TODO distinguish different database errors
+        // TODO apply context and map_err
+        return Err(bad_request_error(
+            "Failed to insert new project, maybe it already exists.",
+        ));
+    };
+    let id = result.last_insert_id();
+    let project = ProjectCreated {
+        id: id as u32,
+        name: data.name.clone(),
+        openstack_id: data.openstack_id.clone(),
+        user_class,
+        // TODO retrieve actual values
+        users: vec![],
+        flavor_groups: vec![],
+    };
+    Ok(HttpResponse::Created()
+        .content_type("application/json")
+        .json(project))
 }
 
 // TODO proper query set and permissions
