af233a689ff52b2cdfd43b6fd47a49b167adbdc9
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-18 15:14:08 +0200

    test(test/project): add e2e_lib_projects_list_returns_own_project
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project.rs
+++ test/tests/user/project.rs
@@ -81,6 +81,44 @@ async fn e2e_lib_project_get_for_own_project_of_admin() {
 }
 
 #[tokio::test]
+async fn e2e_lib_project_list_returns_own_project() {
+    // arrange
+    let server = spawn_app().await;
+    let (user, project, token) = server
+        .setup_test_user_and_project(true)
+        .await
+        .expect("Failed to setup test user and project.");
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let projects = client.project.list().send().unwrap();
+
+        // assert
+        assert_eq!(projects.len(), 1);
+        let project2 = &projects[0];
+        assert_eq!(project.id, project2.id);
+        assert_eq!(project.name, project2.name);
+        assert_eq!(project.openstack_id, project2.openstack_id);
+        assert_eq!(project.user_class, project2.user_class);
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
 async fn e2e_lib_project_create_and_get_works() {
     // Arrange
     let server = spawn_app().await;
