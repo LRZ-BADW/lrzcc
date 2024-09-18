76b0343b9b886d5f87d77f7dbd703fa2fa0527ea
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-18 16:55:24 +0200

    test(test/project): add e2e_lib_project_modify_and_get_works
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project.rs
+++ test/tests/user/project.rs
@@ -222,3 +222,53 @@ async fn e2e_lib_project_create_and_list_works() {
     .await
     .unwrap();
 }
+
+#[tokio::test]
+async fn e2e_lib_project_modify_and_get_works() {
+    // arrange
+    let server = spawn_app().await;
+    let (user, project, token) = server
+        .setup_test_user_and_project(true)
+        .await
+        .expect("Failed to setup test user and project.");
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act and assert 1 - modify
+        let name = random_alphanumeric_string(10);
+        let openstack_id = random_uuid();
+        let user_class = random_number(1..6);
+        let modified = client
+            .project
+            .modify(project.id)
+            .name(name.clone())
+            .openstack_id(openstack_id.clone())
+            .user_class(user_class)
+            .send()
+            .unwrap();
+        assert_eq!(name, modified.name);
+        assert_eq!(openstack_id, modified.openstack_id);
+        assert_eq!(user_class, modified.user_class);
+
+        // act and assert 2 - get
+        let detailed = client.project.get(modified.id).unwrap();
+        assert_eq!(detailed.id, modified.id);
+        assert_eq!(detailed.name, modified.name);
+        assert_eq!(detailed.openstack_id, modified.openstack_id);
+        assert_eq!(detailed.user_class, modified.user_class);
+    })
+    .await
+    .unwrap();
+}
