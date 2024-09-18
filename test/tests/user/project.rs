a65aea618df7dfe9a8c85641b00b25bd124427c0
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-18 15:23:21 +0200

    test(test/project): add e2e_lib_project_create_and_list_works
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project.rs
+++ test/tests/user/project.rs
@@ -167,3 +167,58 @@ async fn e2e_lib_project_create_and_get_works() {
     .await
     .unwrap();
 }
+
+#[tokio::test]
+async fn e2e_lib_project_create_and_list_works() {
+    // arrange
+    let server = spawn_app().await;
+    let (user, project, token) = server
+        .setup_test_user_and_project(true)
+        .await
+        .expect("Failed to setup test user and project.");
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act and assert 1 - create
+        let name = random_alphanumeric_string(10);
+        let openstack_id = random_uuid();
+        let user_class = random_number(1..6);
+        let created = client
+            .project
+            .create(name.clone(), openstack_id.clone())
+            .user_class(user_class)
+            .send()
+            .unwrap();
+        assert_eq!(name, created.name);
+        assert_eq!(openstack_id, created.openstack_id);
+        assert_eq!(user_class, created.user_class);
+
+        // act and assert 2 - list
+        let projects = client.project.list().send().unwrap();
+        assert_eq!(projects.len(), 2);
+        let project2 = &projects[0];
+        assert_eq!(project.id, project2.id);
+        assert_eq!(project.name, project2.name);
+        assert_eq!(project.openstack_id, project2.openstack_id);
+        assert_eq!(project.user_class, project2.user_class);
+        let project3 = &projects[1];
+        assert_eq!(created.id, project3.id);
+        assert_eq!(created.name, project3.name);
+        assert_eq!(created.openstack_id, project3.openstack_id);
+        assert_eq!(created.user_class, project3.user_class);
+    })
+    .await
+    .unwrap();
+}
