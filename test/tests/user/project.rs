d030f32833e56fc83df179d8e70e7d7ecda9871e
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-18 13:48:57 +0200

    test(test/project): remove e2e_lib_project_create_returns_not_found
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project.rs
+++ test/tests/user/project.rs
@@ -44,46 +44,6 @@ async fn e2e_lib_project_create_denies_access_to_normal_user() {
 
 // TODO this is just temporary, a general not found test is better for this
 #[tokio::test]
-async fn e2e_lib_project_create_returns_not_found() {
-    // arrange
-    let server = spawn_app().await;
-    let (user, _project, token) = server
-        .setup_test_user_and_project(true)
-        .await
-        .expect("Failed to setup test user and project.");
-    server
-        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
-        .mount(&server.keystone_server)
-        .await;
-
-    spawn_blocking(move || {
-        // arrange
-        let client = Api::new(
-            format!("{}/api", &server.address),
-            Token::from_str(&token).unwrap(),
-            None,
-            None,
-        )
-        .unwrap();
-
-        // act
-        let name = random_alphanumeric_string(10);
-        let openstack_id = random_uuid();
-        let create = client.project.create(name, openstack_id).send();
-
-        // assert
-        assert!(create.is_err());
-        assert_eq!(
-            create.unwrap_err().to_string(),
-            "This route does not exist."
-        );
-    })
-    .await
-    .unwrap();
-}
-
-// TODO this is just temporary, a general not found test is better for this
-#[tokio::test]
 async fn e2e_lib_project_get_for_own_project_of_admin() {
     // arrange
     let server = spawn_app().await;
