ba5b97a01573855849bc99bf7136cf47f6969eda
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-18 14:43:01 +0200

    test(test/project): add e2e_lib_project_create_and_get_works
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project.rs
+++ test/tests/user/project.rs
@@ -1,5 +1,7 @@
 use lrzcc::{Api, Token};
-use lrzcc_test::{random_alphanumeric_string, random_uuid, spawn_app};
+use lrzcc_test::{
+    random_alphanumeric_string, random_number, random_uuid, spawn_app,
+};
 use std::str::FromStr;
 use tokio::task::spawn_blocking;
 
@@ -42,7 +44,6 @@ async fn e2e_lib_project_create_denies_access_to_normal_user() {
     .unwrap();
 }
 
-// TODO this is just temporary, a general not found test is better for this
 #[tokio::test]
 async fn e2e_lib_project_get_for_own_project_of_admin() {
     // arrange
@@ -74,6 +75,56 @@ async fn e2e_lib_project_get_for_own_project_of_admin() {
         assert_eq!(project.name, project_detailed.name);
         assert_eq!(project.openstack_id, project_detailed.openstack_id);
         assert_eq!(project.user_class, project_detailed.user_class);
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_project_create_and_get_works() {
+    // Arrange
+    let server = spawn_app().await;
+    let (user, _project, token) = server
+        .setup_test_user_and_project(true)
+        .await
+        .expect("Failed to setup test user and project.");
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+
+    spawn_blocking(move || {
+        // Arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // Act and Assert 1
+        let name = random_alphanumeric_string(10);
+        let openstack_id = random_uuid();
+        let user_class = random_number(1..6);
+        let created = client
+            .project
+            .create(name.clone(), openstack_id.clone())
+            .user_class(user_class)
+            .send()
+            .unwrap();
+        assert_eq!(name, created.name);
+        assert_eq!(openstack_id, created.openstack_id);
+        assert_eq!(user_class, created.user_class);
+
+        // Act and Assert 2
+        let detailed = client.project.get(created.id).unwrap();
+        assert_eq!(detailed.id, created.id);
+        assert_eq!(detailed.name, created.name);
+        assert_eq!(detailed.openstack_id, created.openstack_id);
+        assert_eq!(detailed.user_class, created.user_class);
+        assert_eq!(detailed.users.len(), 0);
+        assert_eq!(detailed.flavor_groups.len(), 0);
     })
     .await
     .unwrap();
