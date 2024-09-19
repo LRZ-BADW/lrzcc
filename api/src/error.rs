b3a82e6af481b9c7aa5a21c4d667383f359f62ec
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 21:07:46 +0200

    refactor(api/project): generalize error definitions and move to error module
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/error.rs
+++ api/src/error.rs
@@ -1,6 +1,10 @@
+use actix_web::body::BoxBody;
 use actix_web::error::InternalError;
+use actix_web::http::header::HeaderValue;
+use actix_web::http::{header::CONTENT_TYPE, StatusCode};
 use actix_web::HttpResponse;
-use lrzcc_wire::error::ErrorResponse;
+use actix_web::ResponseError;
+use lrzcc_wire::error::{error_chain_fmt, ErrorResponse};
 
 pub fn unauthorized_error(message: &str) -> actix_web::Error {
     InternalError::from_response(
@@ -44,4 +48,75 @@ pub fn not_found_error(message: &str) -> actix_web::Error {
 
 pub async fn not_found() -> Result<HttpResponse, actix_web::Error> {
     Err(not_found_error("This route does not exist."))
+}
+
+#[derive(thiserror::Error)]
+pub enum NormalApiError {
+    #[error("{0}")]
+    ValidationError(String),
+    #[error("{0}")]
+    AuthorizationError(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for NormalApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for NormalApiError {
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let (status_code, message) = match self {
+            NormalApiError::ValidationError(message) => {
+                (StatusCode::BAD_REQUEST, message.clone())
+            }
+            NormalApiError::AuthorizationError(message) => {
+                (StatusCode::FORBIDDEN, message.clone())
+            }
+            NormalApiError::UnexpectedError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error, contact admin or check logs"
+                    .to_string(),
+            ),
+        };
+        HttpResponse::build(status_code)
+            .insert_header((
+                CONTENT_TYPE,
+                HeaderValue::from_static("application/json"),
+            ))
+            // TODO: handle unwrap
+            .body(
+                serde_json::to_string(&ErrorResponse { detail: message })
+                    .unwrap(),
+            )
+    }
+}
+
+impl From<MinimalApiError> for NormalApiError {
+    fn from(value: MinimalApiError) -> Self {
+        match value {
+            MinimalApiError::ValidationError(message) => {
+                NormalApiError::ValidationError(message)
+            }
+            MinimalApiError::UnexpectedError(error) => {
+                NormalApiError::UnexpectedError(error)
+            }
+        }
+    }
+}
+
+#[derive(thiserror::Error)]
+pub enum MinimalApiError {
+    #[error("{0}")]
+    ValidationError(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for MinimalApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
 }
--- api/src/routes/user/project/create.rs
+++ api/src/routes/user/project/create.rs
@@ -1,57 +1,9 @@
-use actix_web::body::BoxBody;
-use actix_web::http::header::HeaderValue;
-use actix_web::http::{header::CONTENT_TYPE, StatusCode};
+use crate::error::{MinimalApiError, NormalApiError};
 use actix_web::web::{Data, Json, ReqData};
 use actix_web::HttpResponse;
-use actix_web::ResponseError;
 use anyhow::Context;
-use lrzcc_wire::error::{error_chain_fmt, ErrorResponse};
 use lrzcc_wire::user::{Project, ProjectCreateData, ProjectCreated, User};
 use sqlx::{Executor, MySql, MySqlPool, Transaction};
-
-#[derive(thiserror::Error)]
-pub enum ProjectCreateError {
-    #[error("{0}")]
-    ValidationError(String),
-    #[error("{0}")]
-    AuthorizationError(String),
-    #[error(transparent)]
-    UnexpectedError(#[from] anyhow::Error),
-}
-
-impl std::fmt::Debug for ProjectCreateError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl ResponseError for ProjectCreateError {
-    fn error_response(&self) -> HttpResponse<BoxBody> {
-        let (status_code, message) = match self {
-            ProjectCreateError::ValidationError(message) => {
-                (StatusCode::BAD_REQUEST, message.clone())
-            }
-            ProjectCreateError::AuthorizationError(message) => {
-                (StatusCode::FORBIDDEN, message.clone())
-            }
-            ProjectCreateError::UnexpectedError(_) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Internal server error, contact admin or check logs"
-                    .to_string(),
-            ),
-        };
-        HttpResponse::build(status_code)
-            .insert_header((
-                CONTENT_TYPE,
-                HeaderValue::from_static("application/json"),
-            ))
-            // TODO: handle unwrap
-            .body(
-                serde_json::to_string(&ErrorResponse { detail: message })
-                    .unwrap(),
-            )
-    }
-}
 
 pub struct NewProject {
     pub name: String,
@@ -72,35 +24,20 @@ impl TryFrom<ProjectCreateData> for NewProject {
     }
 }
 
-impl From<InsertProjectError> for ProjectCreateError {
-    fn from(value: InsertProjectError) -> Self {
-        match value {
-            InsertProjectError::ValidationError(message) => {
-                ProjectCreateError::ValidationError(message)
-            }
-            InsertProjectError::UnexpectedError(error) => {
-                ProjectCreateError::UnexpectedError(error)
-            }
-        }
-    }
-}
-
 #[tracing::instrument(name = "project_create")]
 pub async fn project_create(
     user: ReqData<User>,
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
     data: Json<ProjectCreateData>,
-) -> Result<HttpResponse, ProjectCreateError> {
+) -> Result<HttpResponse, NormalApiError> {
     if !user.is_staff {
-        return Err(ProjectCreateError::AuthorizationError(
+        return Err(NormalApiError::AuthorizationError(
             "Admin privileges required".to_string(),
         ));
     }
-    let new_project: NewProject = data
-        .0
-        .try_into()
-        .map_err(ProjectCreateError::ValidationError)?;
+    let new_project: NewProject =
+        data.0.try_into().map_err(NormalApiError::ValidationError)?;
     let mut transaction = db_pool
         .begin()
         .await
@@ -124,20 +61,6 @@ pub async fn project_create(
         .json(project_created))
 }
 
-#[derive(thiserror::Error)]
-pub enum InsertProjectError {
-    #[error("{0}")]
-    ValidationError(String),
-    #[error(transparent)]
-    UnexpectedError(#[from] anyhow::Error),
-}
-
-impl std::fmt::Debug for InsertProjectError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
 #[tracing::instrument(
     name = "Insert new project into database",
     skip(new_project, transaction)
@@ -145,7 +68,7 @@ impl std::fmt::Debug for InsertProjectError {
 pub async fn insert_project(
     transaction: &mut Transaction<'_, MySql>,
     new_project: &NewProject,
-) -> Result<u64, InsertProjectError> {
+) -> Result<u64, MinimalApiError> {
     // TODO: MariaDB 10.5 introduced INSERT ... RETURNING
     let query = sqlx::query!(
         r#"
@@ -161,7 +84,7 @@ pub async fn insert_project(
         .await
         .context("Failed to execute insert query")?;
     if result.rows_affected() == 0 {
-        return Err(InsertProjectError::ValidationError(
+        return Err(MinimalApiError::ValidationError(
             "Failed to insert new project, a conflicting entry exists"
                 .to_string(),
         ));
