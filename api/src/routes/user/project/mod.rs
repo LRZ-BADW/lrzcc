5157a4b0fff13cc2c88c3cc154f261c053a43c6f
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 10:10:49 +0200

    refactor(api/project): split off modify submodule
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/mod.rs
+++ api/src/routes/user/project/mod.rs
@@ -1,13 +1,11 @@
 use crate::authentication::require_admin_user;
-use crate::error::{bad_request_error, internal_server_error, not_found_error};
+use crate::error::not_found_error;
 use actix_web::middleware::from_fn;
-use actix_web::web::{
-    delete, get, patch, post, scope, Data, Json, Path, ReqData,
-};
+use actix_web::web::{delete, get, patch, post, scope, Data, Path, ReqData};
 use actix_web::{HttpResponse, Scope};
-use lrzcc_wire::user::{Project, ProjectModifyData, User};
+use lrzcc_wire::user::{Project, User};
 use serde::Deserialize;
-use sqlx::{Executor, FromRow, MySqlPool};
+use sqlx::{FromRow, MySqlPool};
 
 mod create;
 use create::project_create;
@@ -15,6 +13,8 @@ mod list;
 use list::project_list;
 mod get;
 use get::project_get;
+mod modify;
+use modify::project_modify;
 
 // TODO use anyhow and thiserror
 
@@ -44,82 +44,6 @@ struct ProjectRow {
     name: String,
     openstack_id: String,
     user_class: u32,
-}
-
-#[tracing::instrument(name = "project_modify")]
-async fn project_modify(
-    user: ReqData<User>,
-    project: ReqData<Project>,
-    db_pool: Data<MySqlPool>,
-    data: Json<ProjectModifyData>,
-    params: Path<ProjectIdParam>,
-) -> Result<HttpResponse, actix_web::Error> {
-    if data.id != params.project_id {
-        return Err(bad_request_error("ID in URL does not match ID in body"));
-    }
-    let Ok(mut transaction) = db_pool.begin().await else {
-        // TODO there might be other errors as well
-        // TODO apply context and map_err
-        return Err(internal_server_error("Failed to start transaction"));
-    };
-    let query = sqlx::query_as::<_, ProjectRow>(
-        r#"
-        SELECT
-            id,
-            name,
-            openstack_id,
-            user_class
-        FROM user_project
-        WHERE id = ?
-        "#,
-    )
-    .bind(data.id);
-    let Ok(row) = transaction.fetch_one(query).await else {
-        // TODO there might be other errors as well
-        // TODO apply context and map_err
-        return Err(not_found_error("Project with given ID not found"));
-    };
-    let Ok(row) = ProjectRow::from_row(&row) else {
-        return Err(internal_server_error("Failed to parse project row"));
-    };
-    let name = data.name.clone().unwrap_or(row.name);
-    let openstack_id = data.openstack_id.clone().unwrap_or(row.openstack_id);
-    let user_class = data.user_class.unwrap_or(row.user_class);
-    let query = sqlx::query!(
-        r#"
-        UPDATE user_project
-        SET name = ?, openstack_id = ?, user_class = ?
-        WHERE id = ?
-        "#,
-        name,
-        openstack_id,
-        user_class,
-        data.id,
-    );
-    match transaction.execute(query).await {
-        Ok(_) => (),
-        Err(e) => {
-            // TODO distinguish different database errors
-            // TODO apply context and map_err
-            tracing::error!("Failed to update project: {:?}", e);
-            return Err(bad_request_error(
-                "Failed to insert new project, maybe it already exists",
-            ));
-        }
-    };
-    if transaction.commit().await.is_err() {
-        // TODO apply context and map_err
-        return Err(internal_server_error("Failed to commit transaction"));
-    }
-    let project = Project {
-        id: data.id,
-        name,
-        openstack_id,
-        user_class,
-    };
-    Ok(HttpResponse::Ok()
-        .content_type("application/json")
-        .json(project))
 }
 
 #[tracing::instrument(name = "project_delete")]
