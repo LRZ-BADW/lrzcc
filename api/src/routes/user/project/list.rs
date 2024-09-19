d20254ab4a98a8fe08fa192a47ce850673978353
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 09:38:52 +0200

    refactor(api/project): split off list submodule
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ api/src/routes/user/project/list.rs
@@ -0,0 +1,47 @@
+use super::ProjectRow;
+use crate::error::internal_server_error;
+use actix_web::web::{Data, ReqData};
+use actix_web::HttpResponse;
+use lrzcc_wire::user::{Project, User};
+use sqlx::MySqlPool;
+
+// TODO proper query set and permissions
+#[tracing::instrument(name = "project_list")]
+pub async fn project_list(
+    user: ReqData<User>,
+    project: ReqData<Project>,
+    db_pool: Data<MySqlPool>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let Ok(rows) = sqlx::query_as!(
+        ProjectRow,
+        r#"
+        SELECT
+            id,
+            name,
+            openstack_id,
+            user_class
+        FROM user_project
+        "#,
+    )
+    .fetch_all(db_pool.get_ref())
+    .await
+    else {
+        // TODO there might be other errors as well
+        // TODO apply context and map_err
+        return Err(internal_server_error("Failed to retrieve projects"));
+    };
+
+    let projects = rows
+        .into_iter()
+        .map(|r| Project {
+            id: r.id as u32,
+            name: r.name,
+            openstack_id: r.openstack_id,
+            user_class: r.user_class,
+        })
+        .collect::<Vec<_>>();
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .json(projects))
+}
--- api/src/routes/user/project/mod.rs
+++ api/src/routes/user/project/mod.rs
@@ -11,6 +11,8 @@ use sqlx::{Executor, FromRow, MySqlPool};
 
 mod create;
 use create::project_create;
+mod list;
+use list::project_list;
 
 // TODO use anyhow and thiserror
 
@@ -40,47 +42,6 @@ struct ProjectRow {
     name: String,
     openstack_id: String,
     user_class: u32,
-}
-
-// TODO proper query set and permissions
-#[tracing::instrument(name = "project_list")]
-async fn project_list(
-    user: ReqData<User>,
-    project: ReqData<Project>,
-    db_pool: Data<MySqlPool>,
-) -> Result<HttpResponse, actix_web::Error> {
-    let Ok(rows) = sqlx::query_as!(
-        ProjectRow,
-        r#"
-        SELECT
-            id,
-            name,
-            openstack_id,
-            user_class
-        FROM user_project
-        "#,
-    )
-    .fetch_all(db_pool.get_ref())
-    .await
-    else {
-        // TODO there might be other errors as well
-        // TODO apply context and map_err
-        return Err(internal_server_error("Failed to retrieve projects"));
-    };
-
-    let projects = rows
-        .into_iter()
-        .map(|r| Project {
-            id: r.id as u32,
-            name: r.name,
-            openstack_id: r.openstack_id,
-            user_class: r.user_class,
-        })
-        .collect::<Vec<_>>();
-
-    Ok(HttpResponse::Ok()
-        .content_type("application/json")
-        .json(projects))
 }
 
 // TODO proper query set and permissions
