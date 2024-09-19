b8f2125e374588e9b07ff8b87a0ee72ae6d95285
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 10:33:45 +0200

    feat(wire): move error_chain_fmt from lib to wire crate
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/error.rs
+++ wire/src/error.rs
@@ -5,3 +5,16 @@ use tabled::Tabled;
 pub struct ErrorResponse {
     pub detail: String,
 }
+
+pub fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "caused by: {}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
