9f0afae72983f4b2309cc4eaf004560b9030765a
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 09:42:12 +0200

    refactor(api/project): split off get submodule
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/mod.rs
+++ api/src/routes/user/project/mod.rs
@@ -5,7 +5,7 @@ use actix_web::web::{
     delete, get, patch, post, scope, Data, Json, Path, ReqData,
 };
 use actix_web::{HttpResponse, Scope};
-use lrzcc_wire::user::{Project, ProjectDetailed, ProjectModifyData, User};
+use lrzcc_wire::user::{Project, ProjectModifyData, User};
 use serde::Deserialize;
 use sqlx::{Executor, FromRow, MySqlPool};
 
@@ -13,6 +13,8 @@ mod create;
 use create::project_create;
 mod list;
 use list::project_list;
+mod get;
+use get::project_get;
 
 // TODO use anyhow and thiserror
 
@@ -42,51 +44,6 @@ struct ProjectRow {
     name: String,
     openstack_id: String,
     user_class: u32,
-}
-
-// TODO proper query set and permissions
-#[tracing::instrument(name = "project_get")]
-async fn project_get(
-    user: ReqData<User>,
-    project: ReqData<Project>,
-    db_pool: Data<MySqlPool>,
-    params: Path<ProjectIdParam>,
-) -> Result<HttpResponse, actix_web::Error> {
-    let Ok(row) = sqlx::query_as!(
-        ProjectRow,
-        r#"
-        SELECT
-            id,
-            name,
-            openstack_id,
-            user_class
-        FROM user_project AS project
-        WHERE
-            project.id = ?
-        "#,
-        params.project_id,
-    )
-    .fetch_one(db_pool.get_ref())
-    .await
-    else {
-        // TODO there might be other errors as well
-        // TODO apply context and map_err
-        return Err(not_found_error("Project with given ID not found"));
-    };
-
-    let project = ProjectDetailed {
-        id: row.id as u32,
-        name: row.name,
-        openstack_id: row.openstack_id,
-        user_class: row.user_class,
-        // TODO retrieve actual values
-        users: vec![],
-        flavor_groups: vec![],
-    };
-
-    Ok(HttpResponse::Ok()
-        .content_type("application/json")
-        .json(project))
 }
 
 #[tracing::instrument(name = "project_modify")]
