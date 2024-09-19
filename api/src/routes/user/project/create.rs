058384a9f0bdd08f0173623f322053d8b3b44e2d
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 16:48:04 +0200

    feat(api/project): add proper error handling to project create
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/create.rs
+++ api/src/routes/user/project/create.rs
@@ -1,8 +1,84 @@
-use crate::error::bad_request_error;
+use actix_web::body::BoxBody;
+use actix_web::http::header::HeaderValue;
+use actix_web::http::{header::CONTENT_TYPE, StatusCode};
 use actix_web::web::{Data, Json, ReqData};
 use actix_web::HttpResponse;
+use actix_web::ResponseError;
+use anyhow::Context;
+use lrzcc_wire::error::{error_chain_fmt, ErrorResponse};
 use lrzcc_wire::user::{Project, ProjectCreateData, ProjectCreated, User};
-use sqlx::{Executor, MySqlPool};
+use sqlx::{Executor, MySql, MySqlPool, Transaction};
+
+#[derive(thiserror::Error)]
+pub enum ProjectCreateError {
+    #[error("{0}")]
+    ValidationError(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for ProjectCreateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for ProjectCreateError {
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let (status_code, message) = match self {
+            ProjectCreateError::ValidationError(message) => {
+                (StatusCode::BAD_REQUEST, message.clone())
+            }
+            ProjectCreateError::UnexpectedError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error, contact admin or check logs"
+                    .to_string(),
+            ),
+        };
+        HttpResponse::build(status_code)
+            .insert_header((
+                CONTENT_TYPE,
+                HeaderValue::from_static("application/json"),
+            ))
+            // TODO: handle unwrap
+            .body(
+                serde_json::to_string(&ErrorResponse { detail: message })
+                    .unwrap(),
+            )
+    }
+}
+
+pub struct NewProject {
+    pub name: String,
+    pub openstack_id: String,
+    pub user_class: u32,
+}
+
+impl TryFrom<ProjectCreateData> for NewProject {
+    type Error = String;
+
+    fn try_from(data: ProjectCreateData) -> Result<Self, Self::Error> {
+        // TODO really validate data, user_class range, uuid, string length
+        Ok(Self {
+            name: data.name,
+            openstack_id: data.openstack_id,
+            user_class: data.user_class.unwrap_or(1),
+        })
+    }
+}
+
+impl From<InsertProjectError> for ProjectCreateError {
+    fn from(value: InsertProjectError) -> Self {
+        match value {
+            InsertProjectError::ValidationError(message) => {
+                ProjectCreateError::ValidationError(message)
+            }
+            InsertProjectError::UnexpectedError(error) => {
+                ProjectCreateError::UnexpectedError(error)
+            }
+        }
+    }
+}
 
 #[tracing::instrument(name = "project_create")]
 pub async fn project_create(
@@ -10,40 +86,76 @@ pub async fn project_create(
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
     data: Json<ProjectCreateData>,
-) -> Result<HttpResponse, actix_web::Error> {
-    let user_class = data.user_class.unwrap_or(1);
-    // TODO: MariaDB 10.5 introduced INSERT ... RETURNING
-    let query = sqlx::query!(
-        r#"
-        INSERT INTO user_project (name, openstack_id, user_class)
-        VALUES (?, ?, ?)
-        "#,
-        data.name,
-        data.openstack_id,
-        user_class
-    );
-    let result = match db_pool.execute(query).await {
-        Ok(result) => result,
-        Err(e) => {
-            // TODO distinguish different database errors
-            // TODO apply context and map_err
-            tracing::error!("Failed to insert new project: {:?}", e);
-            return Err(bad_request_error(
-                "Failed to insert new project, maybe it already exists",
-            ));
-        }
-    };
-    let id = result.last_insert_id();
-    let project = ProjectCreated {
+) -> Result<HttpResponse, ProjectCreateError> {
+    let new_project: NewProject = data
+        .0
+        .try_into()
+        .map_err(ProjectCreateError::ValidationError)?;
+    let mut transaction = db_pool
+        .begin()
+        .await
+        .context("Failed to acquire a database connection from the pool")?;
+    let id = insert_project(&mut transaction, &new_project).await?;
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit transaction")?;
+    let project_created = ProjectCreated {
         id: id as u32,
-        name: data.name.clone(),
-        openstack_id: data.openstack_id.clone(),
-        user_class,
+        name: new_project.name.clone(),
+        openstack_id: new_project.openstack_id.clone(),
+        user_class: new_project.user_class,
         // TODO retrieve actual values
         users: vec![],
         flavor_groups: vec![],
     };
     Ok(HttpResponse::Created()
         .content_type("application/json")
-        .json(project))
+        .json(project_created))
+}
+
+#[derive(thiserror::Error)]
+pub enum InsertProjectError {
+    #[error("{0}")]
+    ValidationError(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for InsertProjectError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+#[tracing::instrument(
+    name = "Insert new project into database",
+    skip(new_project, transaction)
+)]
+pub async fn insert_project(
+    transaction: &mut Transaction<'_, MySql>,
+    new_project: &NewProject,
+) -> Result<u64, InsertProjectError> {
+    // TODO: MariaDB 10.5 introduced INSERT ... RETURNING
+    let query = sqlx::query!(
+        r#"
+        INSERT IGNORE INTO user_project (name, openstack_id, user_class)
+        VALUES (?, ?, ?)
+        "#,
+        new_project.name,
+        new_project.openstack_id,
+        new_project.user_class
+    );
+    let result = transaction
+        .execute(query)
+        .await
+        .context("Failed to execute insert query")?;
+    if result.rows_affected() == 0 {
+        return Err(InsertProjectError::ValidationError(
+            "Failed to insert new project, a conflicting entry exists"
+                .to_string(),
+        ));
+    }
+    let id = result.last_insert_id();
+    Ok(id)
 }
