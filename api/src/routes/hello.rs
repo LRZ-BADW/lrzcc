a302cfd8f18e18cc781c0dd46545b563e7a337fe
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-19 20:11:06 +0200

    refactor(api/hello): add proper error handling, put admin check in handler
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/hello.rs
+++ api/src/routes/hello.rs
@@ -1,17 +1,18 @@
-use crate::authentication::require_admin_user;
-use actix_web::middleware::from_fn;
+use actix_web::body::BoxBody;
+use actix_web::http::header::HeaderValue;
+use actix_web::http::{header::CONTENT_TYPE, StatusCode};
 use actix_web::web::ReqData;
 use actix_web::web::{get, scope};
+use actix_web::ResponseError;
 use actix_web::{HttpResponse, Scope};
+use lrzcc_wire::error::{error_chain_fmt, ErrorResponse};
 use lrzcc_wire::hello::Hello;
 use lrzcc_wire::user::{Project, User};
 
 pub fn hello_scope() -> Scope {
-    scope("/hello").route("", get().to(hello_user)).service(
-        scope("")
-            .wrap(from_fn(require_admin_user))
-            .route("/admin", get().to(hello_admin)),
-    )
+    scope("/hello")
+        .route("", get().to(hello_user))
+        .route("/admin", get().to(hello_admin))
 }
 
 #[tracing::instrument(name = "hello_user")]
@@ -29,17 +30,54 @@ async fn hello_user(
         })
 }
 
+#[derive(thiserror::Error)]
+pub enum HelloAdminError {
+    #[error("{0}")]
+    AuthorizationError(String),
+}
+
+impl std::fmt::Debug for HelloAdminError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for HelloAdminError {
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let (status_code, message) = match self {
+            HelloAdminError::AuthorizationError(message) => {
+                (StatusCode::FORBIDDEN, message.clone())
+            }
+        };
+        HttpResponse::build(status_code)
+            .insert_header((
+                CONTENT_TYPE,
+                HeaderValue::from_static("application/json"),
+            ))
+            // TODO: handle unwrap
+            .body(
+                serde_json::to_string(&ErrorResponse { detail: message })
+                    .unwrap(),
+            )
+    }
+}
+
 #[tracing::instrument(name = "hello_admin")]
 async fn hello_admin(
     user: ReqData<User>,
     project: ReqData<Project>,
-) -> HttpResponse {
-    HttpResponse::Ok()
+) -> Result<HttpResponse, HelloAdminError> {
+    if !user.is_staff {
+        return Err(HelloAdminError::AuthorizationError(
+            "Admin privileges required".to_string(),
+        ));
+    }
+    Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(Hello {
             message: format!(
                 "Hello, admin {} from project {} with user class {}",
                 user.name, project.name, project.user_class
             ),
-        })
+        }))
 }
