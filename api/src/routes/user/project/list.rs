0cf254b036c6a2da995d3d638bacc47107ec7e89
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-20 14:54:07 +0200

    feat(api/project): add select_all_projects_from_db, and error handling to project_list
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/list.rs
+++ api/src/routes/user/project/list.rs
@@ -1,9 +1,10 @@
 use super::ProjectRow;
-use crate::error::internal_server_error;
+use crate::error::{require_admin_user, NormalApiError, UnexpectedOnlyError};
 use actix_web::web::{Data, ReqData};
 use actix_web::HttpResponse;
+use anyhow::Context;
 use lrzcc_wire::user::{Project, User};
-use sqlx::MySqlPool;
+use sqlx::{Executor, FromRow, MySql, MySqlPool, Transaction};
 
 // TODO proper query set and permissions
 #[tracing::instrument(name = "project_list")]
@@ -12,25 +13,17 @@ pub async fn project_list(
     // TODO: we don't need this right?
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
-) -> Result<HttpResponse, actix_web::Error> {
-    let Ok(rows) = sqlx::query_as!(
-        ProjectRow,
-        r#"
-        SELECT
-            id,
-            name,
-            openstack_id,
-            user_class
-        FROM user_project
-        "#,
-    )
-    .fetch_all(db_pool.get_ref())
-    .await
-    else {
-        // TODO there might be other errors as well
-        // TODO apply context and map_err
-        return Err(internal_server_error("Failed to retrieve projects"));
-    };
+) -> Result<HttpResponse, NormalApiError> {
+    require_admin_user(&user)?;
+    let mut transaction = db_pool
+        .begin()
+        .await
+        .context("Failed to begin transaction")?;
+    let rows = select_all_projects_from_db(&mut transaction).await?;
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit transaction")?;
 
     let projects = rows
         .into_iter()
@@ -45,4 +38,29 @@ pub async fn project_list(
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(projects))
+}
+
+#[tracing::instrument(name = "select_all_projects_from_db", skip(transaction))]
+pub async fn select_all_projects_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+) -> Result<Vec<ProjectRow>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            id,
+            name,
+            openstack_id,
+            user_class
+        FROM user_project
+        "#,
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| ProjectRow::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to project")?;
+    Ok(rows)
 }
