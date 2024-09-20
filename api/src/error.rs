ad42316280e2f71383c2c59c38dca7b16436a521
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-20 14:30:11 +0200

    feat(api/error): add OptionApiError
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/error.rs
+++ api/src/error.rs
@@ -52,6 +52,104 @@ pub async fn not_found() -> Result<HttpResponse, actix_web::Error> {
 }
 
 #[derive(thiserror::Error)]
+pub enum OptionApiError {
+    #[error("{0}")]
+    ValidationError(String),
+    #[error("{0}")]
+    NotFoundError(String),
+    #[error("{0}")]
+    AuthorizationError(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for OptionApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for OptionApiError {
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let (status_code, message) = match self {
+            OptionApiError::ValidationError(message) => {
+                (StatusCode::BAD_REQUEST, message.clone())
+            }
+            OptionApiError::NotFoundError(message) => {
+                (StatusCode::NOT_FOUND, message.clone())
+            }
+            OptionApiError::AuthorizationError(message) => {
+                (StatusCode::FORBIDDEN, message.clone())
+            }
+            OptionApiError::UnexpectedError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error, contact admin or check logs"
+                    .to_string(),
+            ),
+        };
+        HttpResponse::build(status_code)
+            .insert_header((
+                CONTENT_TYPE,
+                HeaderValue::from_static("application/json"),
+            ))
+            // TODO: handle unwrap
+            .body(
+                serde_json::to_string(&ErrorResponse { detail: message })
+                    .unwrap(),
+            )
+    }
+}
+
+impl From<NormalApiError> for OptionApiError {
+    fn from(value: NormalApiError) -> Self {
+        match value {
+            NormalApiError::ValidationError(message) => {
+                Self::ValidationError(message)
+            }
+            NormalApiError::AuthorizationError(message) => {
+                Self::AuthorizationError(message)
+            }
+            NormalApiError::UnexpectedError(error) => {
+                Self::UnexpectedError(error)
+            }
+        }
+    }
+}
+
+impl From<MinimalApiError> for OptionApiError {
+    fn from(value: MinimalApiError) -> Self {
+        match value {
+            MinimalApiError::ValidationError(message) => {
+                Self::ValidationError(message)
+            }
+            MinimalApiError::UnexpectedError(error) => {
+                Self::UnexpectedError(error)
+            }
+        }
+    }
+}
+
+impl From<UnexpectedOnlyError> for OptionApiError {
+    fn from(value: UnexpectedOnlyError) -> Self {
+        match value {
+            UnexpectedOnlyError::UnexpectedError(message) => {
+                Self::UnexpectedError(message)
+            }
+        }
+    }
+}
+
+impl From<AuthOnlyError> for OptionApiError {
+    fn from(value: AuthOnlyError) -> Self {
+        match value {
+            AuthOnlyError::AuthorizationError(message) => {
+                Self::AuthorizationError(message)
+            }
+        }
+    }
+}
+
+#[derive(thiserror::Error)]
 pub enum NormalApiError {
     #[error("{0}")]
     ValidationError(String),
