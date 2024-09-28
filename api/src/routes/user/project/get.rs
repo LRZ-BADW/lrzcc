16667516d3c43f6b67a2440a44e3e812e2cee3b7
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-28 19:48:53 +0200

    docs(api/project): remove unneede todo comment
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/get.rs
+++ api/src/routes/user/project/get.rs
@@ -13,7 +13,6 @@ use sqlx::{Executor, FromRow, MySql, MySqlPool, Transaction};
 #[tracing::instrument(name = "project_get")]
 pub async fn project_get(
     user: ReqData<User>,
-    // TODO: we don't need this right?
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
     params: Path<ProjectIdParam>,
