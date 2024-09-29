2b6ed63a1744b0322b3a738146a63bd93aaba1b0
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-29 12:57:30 +0200

    test(test/project/get): check flavor_groups field of ProjectDetailed response
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- test/tests/user/project/get.rs
+++ test/tests/user/project/get.rs
@@ -120,7 +120,8 @@ async fn e2e_lib_admin_can_get_own_project() {
         assert_eq!(project.name, project_detailed.name);
         assert_eq!(project.openstack_id, project_detailed.openstack_id);
         assert_eq!(project.user_class, project_detailed.user_class);
-        // TODO: check users and flavor_groups
+        // TODO: check users
+        assert_eq!(project_detailed.flavor_groups.len(), 0);
     })
     .await
     .unwrap();
@@ -165,7 +166,8 @@ async fn e2e_lib_admin_can_get_other_project() {
         assert_eq!(project2.name, project_detailed.name);
         assert_eq!(project2.openstack_id, project_detailed.openstack_id);
         assert_eq!(project2.user_class, project_detailed.user_class);
-        // TODO: check users and flavor_groups
+        // TODO: check users
+        assert_eq!(project_detailed.flavor_groups.len(), 0);
     })
     .await
     .unwrap();
