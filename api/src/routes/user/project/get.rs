8a1ef5c703c88a036e7e6986d99efb3ffe9bc98e
Sandro-Alessio Gierens <sandro@gierens.de>
2024-09-29 21:02:19 +0200

    feat(api/project/get): add select_minimal_flavor_groups_by_project_id_from_db
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/user/project/get.rs
+++ api/src/routes/user/project/get.rs
@@ -6,6 +6,7 @@ use crate::error::{
 use actix_web::web::{Data, Path, ReqData};
 use actix_web::HttpResponse;
 use anyhow::Context;
+use lrzcc_wire::resources::FlavorGroupMinimal;
 use lrzcc_wire::user::{
     Project, ProjectDetailed, ProjectRetrieved, User, UserMinimal,
 };
@@ -128,6 +129,35 @@ pub async fn select_minimal_users_by_project_id_from_db(
         .context("Failed to execute select query")?
         .into_iter()
         .map(|r| UserMinimal::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to project")?;
+    Ok(rows)
+}
+
+#[tracing::instrument(
+    name = "select_minimal_flavor_groups_by_project_id_from_db",
+    skip(transaction)
+)]
+pub async fn select_minimal_flavor_groups_by_project_id_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    project_id: u64,
+) -> Result<Vec<FlavorGroupMinimal>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            id,
+            name
+        FROM resources_flavorgroup
+        WHERE project_id = ?
+        "#,
+        project_id
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| FlavorGroupMinimal::from_row(&r))
         .collect::<Result<Vec<_>, _>>()
         .context("Failed to convert row to project")?;
     Ok(rows)
