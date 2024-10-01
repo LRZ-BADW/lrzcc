d3ad45efd655494d6f129f9f9456a5be8d1b5be2
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-01 11:35:37 +0200

    feat(scripts): improve build_container.sh, error handling, and optional publish
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+VERSION=0.2.0
+
 docker build \
-    --tag gierens/lrzcc:v0.1.0 \
-    --tag gierens/lrzcc:latest \
+    --tag "gierens/lrzcc:v${VERSION}" \
+    --tag "gierens/lrzcc:latest" \
     --file Dockerfile \
     .
+
+if [ $? -eq 0 ]; then
+    echo "Successfully built the container"
+else
+    echo "Failed to build the container"
+fi
+
+read -p "Publish container? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker publish "gierens/lrzcc:v${VERSION}"
+    docker publish "gierens/lrzcc:latest"
+fi
