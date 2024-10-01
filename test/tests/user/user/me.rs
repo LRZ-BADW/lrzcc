07e1d223d5ebf36e84114091886f29127a8eefad
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-01 14:29:10 +0200

    test(test/user): add e2e_lib_user_me_returns_own_user
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ test/tests/user/user/me.rs
@@ -0,0 +1,45 @@
+use lrzcc::{Api, Token};
+use lrzcc_test::spawn_app;
+use std::str::FromStr;
+use tokio::task::spawn_blocking;
+
+#[tokio::test]
+async fn e2e_lib_user_me_returns_own_user() {
+    // arrange
+    let server = spawn_app().await;
+    let (user, project, token) = server
+        .setup_test_user_and_project(false)
+        .await
+        .expect("Failed to setup test user and project.");
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let me = client.user.me().unwrap();
+
+        // assert
+        assert_eq!(me.id, user.id);
+        assert_eq!(me.name, user.name);
+        assert_eq!(me.openstack_id, user.openstack_id);
+        assert_eq!(me.role, user.role);
+        assert_eq!(me.is_staff, user.is_staff);
+        assert_eq!(me.is_active, user.is_active);
+        assert_eq!(me.project.id, project.id);
+        assert_eq!(me.project.name, project.name);
+        assert_eq!(me.project_name, project.name);
+    })
+    .await
+    .unwrap();
+}
