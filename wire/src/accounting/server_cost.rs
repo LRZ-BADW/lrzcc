b9045071251ba605f0da0dba75d6373697eb9eb7
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-07 14:58:09 +0200

    feat(wire/server_cost): derive PartialEq for all response structs
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/accounting/server_cost.rs
+++ wire/src/accounting/server_cost.rs
@@ -3,32 +3,32 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use tabled::Tabled;
 
-#[derive(Clone, Debug, Deserialize, Serialize, Tabled)]
+#[derive(Clone, Debug, Deserialize, Serialize, Tabled, PartialEq)]
 pub struct ServerCostSimple {
     pub total: f64,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct ServerCostServer {
     pub total: f64,
     pub flavors: HashMap<String, f64>,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct ServerCostUser {
     pub total: f64,
     pub flavors: HashMap<String, f64>,
     pub servers: HashMap<String, ServerCostServer>,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct ServerCostProject {
     pub total: f64,
     pub flavors: HashMap<String, f64>,
     pub users: HashMap<String, ServerCostUser>,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct ServerCostAll {
     pub total: f64,
     pub flavors: HashMap<String, f64>,
