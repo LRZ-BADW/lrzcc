2fd6a6cd4626b0642d8425f6e227b585c985dec6
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-10 10:57:02 +0200

    docs(migrations): revise todo comments in create_user_project_table
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/migrations/20240913084652_create_user_project_table.sql
+++ api/migrations/20240913084652_create_user_project_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `user_project` (
-    -- TODO make this unsigned
+    -- TODO: make this unsigned
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    -- TODO shorten to 36 chars or maybe use uuid type
+    -- TODO: shorten to 36 chars or maybe use uuid type
     `openstack_id` varchar(255) NOT NULL,
-    -- TODO replace by enum
+    -- TODO: replace by enum
     `user_class` smallint(5) unsigned NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `name` (`name`),
