d397dbdbdb5ba8521ab165471e3c2934633e68fe
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-10 23:21:49 +0200

    feat(api): add budgeting::user_budget with user_budget_delete handler
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/budgeting/mod.rs
+++ api/src/routes/budgeting/mod.rs
@@ -3,7 +3,11 @@ use actix_web::Scope;
 
 mod project_budget;
 use project_budget::project_budgets_scope;
+mod user_budget;
+use user_budget::user_budgets_scope;
 
 pub fn budgeting_scope() -> Scope {
-    scope("/budgeting").service(project_budgets_scope())
+    scope("/budgeting")
+        .service(project_budgets_scope())
+        .service(user_budgets_scope())
 }
---
+++ api/src/routes/budgeting/user_budget/delete.rs
@@ -0,0 +1,56 @@
+use crate::authorization::require_admin_user;
+use crate::error::{MinimalApiError, NormalApiError};
+use actix_web::web::{Data, Path, ReqData};
+use actix_web::HttpResponse;
+use anyhow::Context;
+use lrzcc_wire::user::{Project, User};
+use sqlx::{Executor, MySql, MySqlPool, Transaction};
+
+use super::UserBudgetIdParam;
+
+#[tracing::instrument(name = "user_budget_delete")]
+pub async fn user_budget_delete(
+    user: ReqData<User>,
+    // TODO: we don't need this right?
+    project: ReqData<Project>,
+    db_pool: Data<MySqlPool>,
+    params: Path<UserBudgetIdParam>,
+) -> Result<HttpResponse, NormalApiError> {
+    require_admin_user(&user)?;
+    let mut transaction = db_pool
+        .begin()
+        .await
+        .context("Failed to begin transaction")?;
+    delete_user_budget_from_db(&mut transaction, params.user_budget_id as u64)
+        .await?;
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit transaction")?;
+    Ok(HttpResponse::NoContent().finish())
+}
+
+#[tracing::instrument(name = "delete_user_budget_from_db", skip(transaction))]
+async fn delete_user_budget_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    user_budget_id: u64,
+) -> Result<(), MinimalApiError> {
+    let query = sqlx::query!(
+        r#"
+        DELETE IGNORE FROM budgeting_userbudget
+        WHERE id = ?
+        "#,
+        user_budget_id
+    );
+    let result = transaction
+        .execute(query)
+        .await
+        .context("Failed to execute delete query")?;
+    if result.rows_affected() == 0 {
+        return Err(MinimalApiError::ValidationError(
+            // TODO: test that this message is really correct
+            "Failed to delete user budget.".to_string(),
+        ));
+    }
+    Ok(())
+}
