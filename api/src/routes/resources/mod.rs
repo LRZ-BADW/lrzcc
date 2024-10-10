f2967018935105f06b5d04adc574105e8f2e1c88
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-10 21:52:21 +0200

    feat(api): add resources::flavor module with flavor_delete handler
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/resources/mod.rs
+++ api/src/routes/resources/mod.rs
@@ -3,7 +3,11 @@ use actix_web::Scope;
 
 mod flavor_group;
 use flavor_group::flavor_groups_scope;
+mod flavor;
+use flavor::flavors_scope;
 
 pub fn resources_scope() -> Scope {
-    scope("/resources").service(flavor_groups_scope())
+    scope("/resources")
+        .service(flavor_groups_scope())
+        .service(flavors_scope())
 }
