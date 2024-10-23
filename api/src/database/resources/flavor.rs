815d185746914a6c9d936f8e10e566c5f7bb4393
Sandro-Alessio Gierens <sandro@gierens.de>
2024-10-23 10:46:14 +0200

    feat(api/database/resources): add select_flavor_from_db
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/resources/flavor.rs
+++ api/src/database/resources/flavor.rs
@@ -1,5 +1,6 @@
 use crate::error::{NotFoundOrUnexpectedApiError, UnexpectedOnlyError};
 use anyhow::Context;
+use lrzcc_wire::resources::Flavor;
 use sqlx::{Executor, FromRow, MySql, Transaction};
 
 #[tracing::instrument(
@@ -46,5 +47,43 @@ pub async fn select_flavor_name_from_db(
         .await?
         .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
             "User with given ID not found".to_string(),
+        ))
+}
+
+#[tracing::instrument(name = "select_maybe_flavor_from_db", skip(transaction))]
+pub async fn select_maybe_flavor_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    flavor_id: u64,
+) -> Result<Option<Flavor>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT id, name, openstack_id, weight, group_id
+        FROM resources_flavor
+        WHERE id = ?
+        "#,
+        flavor_id
+    );
+    let row = transaction
+        .fetch_optional(query)
+        .await
+        .context("Failed to execute select query")?;
+    // TODO: isn't there a nicer way to write this?
+    Ok(match row {
+        Some(row) => {
+            Some(Flavor::from_row(&row).context("Failed to parse flavor row")?)
+        }
+        None => None,
+    })
+}
+
+#[tracing::instrument(name = "select_flavor_from_db", skip(transaction))]
+pub async fn select_flavor_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    flavor_id: u64,
+) -> Result<Flavor, NotFoundOrUnexpectedApiError> {
+    select_maybe_flavor_from_db(transaction, flavor_id)
+        .await?
+        .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
+            "Flavor  with given ID not found".to_string(),
         ))
 }
