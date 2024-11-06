c3f2cffcf72e95e335c23d01d767b91d050dfb18
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-06 12:17:43 +0100

    feat(api/accounting): require admin user for  server_state_get
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/accounting/server_state/get.rs
+++ api/src/routes/accounting/server_state/get.rs
@@ -1,6 +1,7 @@
 use super::ServerStateIdParam;
+use crate::authorization::require_admin_user;
 use crate::database::accounting::server_state::select_server_state_from_db;
-use crate::error::NotFoundOrUnexpectedApiError;
+use crate::error::OptionApiError;
 use actix_web::web::{Data, Path, ReqData};
 use actix_web::HttpResponse;
 use anyhow::Context;
@@ -14,7 +15,9 @@ pub async fn server_state_get(
     project: ReqData<Project>,
     db_pool: Data<MySqlPool>,
     params: Path<ServerStateIdParam>,
-) -> Result<HttpResponse, NotFoundOrUnexpectedApiError> {
+    // TODO: is the ValidationError variant ever used?
+) -> Result<HttpResponse, OptionApiError> {
+    require_admin_user(&user)?;
     let mut transaction = db_pool
         .begin()
         .await
@@ -29,7 +32,7 @@ pub async fn server_state_get(
         .await
         .context("Failed to commit transaction")?;
     if server_state.user != user.id && !user.is_staff {
-        return Err(NotFoundOrUnexpectedApiError::NotFoundError(
+        return Err(OptionApiError::NotFoundError(
             "Server state not found".to_string(),
         ));
     }
