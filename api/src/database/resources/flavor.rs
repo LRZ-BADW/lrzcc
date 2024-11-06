ca63fed65487c5595d3f9f2e21e70f381889e356
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-06 16:04:51 +0100

    feat(api/database/resources): add select_flavor_detail_from_db
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/resources/flavor.rs
+++ api/src/database/resources/flavor.rs
@@ -1,6 +1,8 @@
 use crate::error::{NotFoundOrUnexpectedApiError, UnexpectedOnlyError};
 use anyhow::Context;
-use lrzcc_wire::resources::{Flavor, FlavorMinimal};
+use lrzcc_wire::resources::{
+    Flavor, FlavorDetailed, FlavorGroupMinimal, FlavorMinimal,
+};
 use sqlx::{Executor, FromRow, MySql, Transaction};
 
 #[tracing::instrument(
@@ -115,4 +117,72 @@ pub async fn select_minimal_flavors_by_group_from_db(
         .collect::<Result<Vec<_>, _>>()
         .context("Failed to convert row to flavor")?;
     Ok(rows)
+}
+
+#[tracing::instrument(
+    name = "select_maybe_user_detail_from_db",
+    skip(transaction)
+)]
+pub async fn select_maybe_flavor_detail_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    flavor_id: u64,
+) -> Result<Option<FlavorDetailed>, UnexpectedOnlyError> {
+    #[derive(FromRow)]
+    pub struct FlavorDb {
+        pub id: u32,
+        pub name: String,
+        pub openstack_id: String, // UUIDv4
+        pub group_id: Option<u32>,
+        pub group_name: Option<String>,
+        pub weight: u32,
+    }
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            f.id AS id,
+            f.name AS name,
+            f.openstack_id AS openstack_id,
+            g.id AS group_id,
+            g.name AS group_name,
+            f.weight AS weight
+        FROM resources_flavor AS f, resources_flavorgroup AS g
+        WHERE
+            f.group_id = g.id AND
+            f.id = ?
+        "#,
+        flavor_id
+    );
+    let row = transaction
+        .fetch_optional(query)
+        .await
+        .context("Failed to execute select query")?;
+    let flavor = match row {
+        Some(row) => {
+            FlavorDb::from_row(&row).context("Failed to parse flavor row")?
+        }
+        None => return Ok(None),
+    };
+    Ok(Some(FlavorDetailed {
+        id: flavor.id,
+        name: flavor.name,
+        openstack_id: flavor.openstack_id,
+        group: match (flavor.group_id, flavor.group_name.clone()) {
+            (Some(id), Some(name)) => Some(FlavorGroupMinimal { id, name }),
+            _ => None,
+        },
+        group_name: flavor.group_name,
+        weight: flavor.weight,
+    }))
+}
+
+#[tracing::instrument(name = "select_user_detail_from_db", skip(transaction))]
+pub async fn select_flavor_detail_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+) -> Result<FlavorDetailed, NotFoundOrUnexpectedApiError> {
+    select_maybe_flavor_detail_from_db(transaction, user_id)
+        .await?
+        .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
+            "Flavor with given ID or linked project not found".to_string(),
+        ))
 }
