06f8c853a091b83afd6560b18f29fd1a9441206d
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-06 11:06:20 +0100

    feat(api/database): add select_project_minimal_from_db
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/user/project.rs
+++ api/src/database/user/project.rs
@@ -1,6 +1,6 @@
 use crate::error::{NotFoundOrUnexpectedApiError, UnexpectedOnlyError};
 use anyhow::Context;
-use lrzcc_wire::user::Project;
+use lrzcc_wire::user::{Project, ProjectMinimal};
 use sqlx::{Executor, FromRow, MySql, Transaction};
 
 #[tracing::instrument(name = "select_maybe_project_from_db", skip(transaction))]
@@ -39,6 +39,50 @@ pub async fn select_project_from_db(
     project_id: u64,
 ) -> Result<Project, NotFoundOrUnexpectedApiError> {
     select_maybe_project_from_db(transaction, project_id)
+        .await?
+        .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
+            "Project with given ID not found".to_string(),
+        ))
+}
+
+#[tracing::instrument(
+    name = "select_maybe_project_minimal_from_db",
+    skip(transaction)
+)]
+pub async fn select_maybe_project_minimal_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    project_id: u64,
+) -> Result<Option<ProjectMinimal>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT id, name
+        FROM user_project AS project
+        WHERE project.id = ?
+        "#,
+        project_id
+    );
+    let row = transaction
+        .fetch_optional(query)
+        .await
+        .context("Failed to execute select query")?;
+    Ok(match row {
+        Some(row) => Some(
+            ProjectMinimal::from_row(&row)
+                .context("Failed to parse project row")?,
+        ),
+        None => None,
+    })
+}
+
+#[tracing::instrument(
+    name = "select_project_minimal_from_db",
+    skip(transaction)
+)]
+pub async fn select_project_minimal_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    project_id: u64,
+) -> Result<ProjectMinimal, NotFoundOrUnexpectedApiError> {
+    select_maybe_project_minimal_from_db(transaction, project_id)
         .await?
         .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
             "Project with given ID not found".to_string(),
