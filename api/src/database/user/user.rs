bfa9fe01adf96ad9fb86fe68e8d281deb0fa0e41
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-12 10:29:00 +0100

    refactor(api/routes): move user list select functions to database module
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/user/user.rs
+++ api/src/database/user/user.rs
@@ -1,5 +1,6 @@
 use crate::error::{NotFoundOrUnexpectedApiError, UnexpectedOnlyError};
 use anyhow::Context;
+use lrzcc_wire::user::User;
 use sqlx::{Executor, FromRow, MySql, Transaction};
 
 #[tracing::instrument(
@@ -47,4 +48,106 @@ pub async fn select_user_name_from_db(
         .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
             "User with given ID not found".to_string(),
         ))
+}
+
+#[tracing::instrument(name = "select_all_users_from_db", skip(transaction))]
+pub async fn select_all_users_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+) -> Result<Vec<User>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            user.id AS id,
+            user.name AS name,
+            user.openstack_id AS openstack_id,
+            user.role AS role,
+            project.id as project,
+            project.name AS project_name,
+            user.is_staff AS is_staff,
+            user.is_active AS is_active
+        FROM user_user AS user, user_project AS project
+        WHERE
+            user.project_id = project.id
+        "#,
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| User::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to project")?;
+    Ok(rows)
+}
+
+#[tracing::instrument(
+    name = "select_users_by_project_from_db",
+    skip(transaction)
+)]
+pub async fn select_users_by_project_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    project_id: u64,
+) -> Result<Vec<User>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            user.id AS id,
+            user.name AS name,
+            user.openstack_id AS openstack_id,
+            user.role AS role,
+            project.id as project,
+            project.name AS project_name,
+            user.is_staff AS is_staff,
+            user.is_active AS is_active
+        FROM user_user AS user, user_project AS project
+        WHERE
+            user.project_id = project.id AND
+            user.project_id = ?
+        "#,
+        project_id
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| User::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to project")?;
+    Ok(rows)
+}
+
+#[tracing::instrument(name = "select_users_by_id_db", skip(transaction))]
+pub async fn select_users_by_id_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+) -> Result<Vec<User>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            user.id AS id,
+            user.name AS name,
+            user.openstack_id AS openstack_id,
+            user.role AS role,
+            project.id as project,
+            project.name AS project_name,
+            user.is_staff AS is_staff,
+            user.is_active AS is_active
+        FROM user_user AS user, user_project AS project
+        WHERE
+            user.project_id = project.id AND
+            user.id = ?
+        "#,
+        user_id
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| User::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to project")?;
+    Ok(rows)
 }
--- api/src/routes/user/user/list.rs
+++ api/src/routes/user/user/list.rs
@@ -1,10 +1,14 @@
 use crate::authorization::{require_admin_user, require_master_user};
-use crate::error::{NormalApiError, UnexpectedOnlyError};
+use crate::database::user::user::{
+    select_all_users_from_db, select_users_by_id_from_db,
+    select_users_by_project_from_db,
+};
+use crate::error::NormalApiError;
 use actix_web::web::{Data, Query, ReqData};
 use actix_web::HttpResponse;
 use anyhow::Context;
 use lrzcc_wire::user::{Project, User, UserListParams};
-use sqlx::{Executor, FromRow, MySql, MySqlPool, Transaction};
+use sqlx::MySqlPool;
 
 #[tracing::instrument(name = "user_list")]
 pub async fn user_list(
@@ -34,106 +38,4 @@ pub async fn user_list(
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .json(users))
-}
-
-#[tracing::instrument(name = "select_all_users_from_db", skip(transaction))]
-pub async fn select_all_users_from_db(
-    transaction: &mut Transaction<'_, MySql>,
-) -> Result<Vec<User>, UnexpectedOnlyError> {
-    let query = sqlx::query!(
-        r#"
-        SELECT
-            user.id AS id,
-            user.name AS name,
-            user.openstack_id AS openstack_id,
-            user.role AS role,
-            project.id as project,
-            project.name AS project_name,
-            user.is_staff AS is_staff,
-            user.is_active AS is_active
-        FROM user_user AS user, user_project AS project
-        WHERE
-            user.project_id = project.id
-        "#,
-    );
-    let rows = transaction
-        .fetch_all(query)
-        .await
-        .context("Failed to execute select query")?
-        .into_iter()
-        .map(|r| User::from_row(&r))
-        .collect::<Result<Vec<_>, _>>()
-        .context("Failed to convert row to project")?;
-    Ok(rows)
-}
-
-#[tracing::instrument(
-    name = "select_users_by_project_from_db",
-    skip(transaction)
-)]
-pub async fn select_users_by_project_from_db(
-    transaction: &mut Transaction<'_, MySql>,
-    project_id: u64,
-) -> Result<Vec<User>, UnexpectedOnlyError> {
-    let query = sqlx::query!(
-        r#"
-        SELECT
-            user.id AS id,
-            user.name AS name,
-            user.openstack_id AS openstack_id,
-            user.role AS role,
-            project.id as project,
-            project.name AS project_name,
-            user.is_staff AS is_staff,
-            user.is_active AS is_active
-        FROM user_user AS user, user_project AS project
-        WHERE
-            user.project_id = project.id AND
-            user.project_id = ?
-        "#,
-        project_id
-    );
-    let rows = transaction
-        .fetch_all(query)
-        .await
-        .context("Failed to execute select query")?
-        .into_iter()
-        .map(|r| User::from_row(&r))
-        .collect::<Result<Vec<_>, _>>()
-        .context("Failed to convert row to project")?;
-    Ok(rows)
-}
-
-#[tracing::instrument(name = "select_users_by_id_db", skip(transaction))]
-pub async fn select_users_by_id_from_db(
-    transaction: &mut Transaction<'_, MySql>,
-    user_id: u64,
-) -> Result<Vec<User>, UnexpectedOnlyError> {
-    let query = sqlx::query!(
-        r#"
-        SELECT
-            user.id AS id,
-            user.name AS name,
-            user.openstack_id AS openstack_id,
-            user.role AS role,
-            project.id as project,
-            project.name AS project_name,
-            user.is_staff AS is_staff,
-            user.is_active AS is_active
-        FROM user_user AS user, user_project AS project
-        WHERE
-            user.project_id = project.id AND
-            user.id = ?
-        "#,
-        user_id
-    );
-    let rows = transaction
-        .fetch_all(query)
-        .await
-        .context("Failed to execute select query")?
-        .into_iter()
-        .map(|r| User::from_row(&r))
-        .collect::<Result<Vec<_>, _>>()
-        .context("Failed to convert row to project")?;
-    Ok(rows)
 }
