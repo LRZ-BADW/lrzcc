bf2dfbb48abd15cd154942e18dd2074b6ca6e7ec
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-15 16:47:44 +0100

    feat(api/database): add select_all_flavor_prices_from_db function
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/pricing/flavor_price.rs
+++ api/src/database/pricing/flavor_price.rs
@@ -4,6 +4,16 @@ use chrono::{DateTime, Utc};
 use lrzcc_wire::pricing::FlavorPrice;
 use sqlx::{Executor, FromRow, MySql, Transaction};
 
+#[derive(FromRow)]
+pub struct FlavorPriceRow {
+    pub id: u32,
+    pub flavor: u32,
+    pub flavor_name: String,
+    pub user_class: u32,
+    pub unit_price: f64,
+    pub start_time: DateTime<Utc>,
+}
+
 #[tracing::instrument(
     name = "select_maybe_flavor_price_from_db",
     skip(transaction)
@@ -12,15 +22,6 @@ pub async fn select_maybe_flavor_price_from_db(
     transaction: &mut Transaction<'_, MySql>,
     flavor_price_id: u64,
 ) -> Result<Option<FlavorPrice>, UnexpectedOnlyError> {
-    #[derive(FromRow)]
-    pub struct Row {
-        pub id: u32,
-        pub flavor: u32,
-        pub flavor_name: String,
-        pub user_class: u32,
-        pub unit_price: f64,
-        pub start_time: DateTime<Utc>,
-    }
     let query = sqlx::query!(
         r#"
         SELECT
@@ -45,7 +46,7 @@ pub async fn select_maybe_flavor_price_from_db(
         .context("Failed to execute select query")?;
     Ok(match row {
         Some(row) => {
-            let row = Row::from_row(&row)
+            let row = FlavorPriceRow::from_row(&row)
                 .context("Failed to parse flavor price row")?;
             Some(FlavorPrice {
                 id: row.id,
@@ -70,4 +71,48 @@ pub async fn select_flavor_price_from_db(
         .ok_or(NotFoundOrUnexpectedApiError::NotFoundError(
             "Flavor price with given ID not found".to_string(),
         ))
+}
+
+#[tracing::instrument(
+    name = "select_all_flavor_prices_from_db",
+    skip(transaction)
+)]
+pub async fn select_all_flavor_prices_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+) -> Result<Vec<FlavorPrice>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT
+            p.id,
+            p.flavor_id as flavor,
+            f.name as flavor_name, 
+            p.user_class as user_class,
+            p.unit_price as unit_price,
+            p.start_time as start_time
+        FROM
+            pricing_flavorprice as p,
+            resources_flavor as f
+        WHERE
+            p.flavor_id = f.id
+        "#,
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| FlavorPriceRow::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to flavor price")?
+        .into_iter()
+        .map(|row| FlavorPrice {
+            id: row.id,
+            flavor: row.flavor,
+            flavor_name: row.flavor_name,
+            user_class: row.user_class,
+            unit_price: row.unit_price,
+            start_time: row.start_time.fixed_offset(),
+        })
+        .collect();
+    Ok(rows)
 }
