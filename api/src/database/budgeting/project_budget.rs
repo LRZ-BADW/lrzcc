9202c2345610dededf67c8ceb52ef6c6e98334a6
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-15 16:05:25 +0100

    feat(api/database): add select_project_budget_by_project_from_db function
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/database/budgeting/project_budget.rs
+++ api/src/database/budgeting/project_budget.rs
@@ -76,6 +76,35 @@ pub async fn select_all_project_budgets_from_db(
 }
 
 #[tracing::instrument(
+    name = "select_project_budgets_by_project_from_db",
+    skip(transaction)
+)]
+pub async fn select_project_budgets_by_project_from_db(
+    transaction: &mut Transaction<'_, MySql>,
+    project_id: u64,
+) -> Result<Vec<ProjectBudget>, UnexpectedOnlyError> {
+    let query = sqlx::query!(
+        r#"
+        SELECT b.id, p.id as project, p.name as project_name, b.year, b.amount
+        FROM budgeting_projectbudget as b, user_project as p
+        WHERE
+            b.project_id = p.id AND
+            p.id = ?
+        "#,
+        project_id
+    );
+    let rows = transaction
+        .fetch_all(query)
+        .await
+        .context("Failed to execute select query")?
+        .into_iter()
+        .map(|r| ProjectBudget::from_row(&r))
+        .collect::<Result<Vec<_>, _>>()
+        .context("Failed to convert row to project")?;
+    Ok(rows)
+}
+
+#[tracing::instrument(
     name = "select_project_budgets_by_year_from_db",
     skip(transaction)
 )]
