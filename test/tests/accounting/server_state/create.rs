3b961c4814b29336af45b8cf4cabfecdb7548091
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-22 23:38:08 +0100

    test(test/accounting): add server state create tests
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ test/tests/accounting/server_state/create.rs
@@ -0,0 +1,324 @@
+use chrono::{DateTime, FixedOffset, Utc};
+use lrzcc::{Api, Token};
+use lrzcc_test::{random_alphanumeric_string, random_uuid, spawn_app};
+use std::str::FromStr;
+use tokio::task::spawn_blocking;
+
+#[tokio::test]
+async fn e2e_lib_server_state_create_denies_access_to_normal_user() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(0, 0, 1)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.normals[0].user.clone();
+    let token = test_project.normals[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let begin = DateTime::<FixedOffset>::from(Utc::now());
+        let instance_id = random_uuid();
+        let instance_name = random_alphanumeric_string(10);
+        let status = "ACTIVE".to_string();
+        let create = client
+            .server_state
+            .create(
+                begin,
+                instance_id,
+                instance_name,
+                flavor.id,
+                status,
+                user.id,
+            )
+            .send();
+
+        // assert
+        assert!(create.is_err());
+        assert_eq!(
+            create.unwrap_err().to_string(),
+            format!("Admin privileges required")
+        );
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_server_state_create_denies_access_to_master_user() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(0, 1, 0)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.masters[0].user.clone();
+    let token = test_project.masters[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let begin = DateTime::<FixedOffset>::from(Utc::now());
+        let instance_id = random_uuid();
+        let instance_name = random_alphanumeric_string(10);
+        let status = "ACTIVE".to_string();
+        let create = client
+            .server_state
+            .create(
+                begin,
+                instance_id,
+                instance_name,
+                flavor.id,
+                status,
+                user.id,
+            )
+            .send();
+
+        // assert
+        assert!(create.is_err());
+        assert_eq!(
+            create.unwrap_err().to_string(),
+            format!("Admin privileges required")
+        );
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_server_state_create_works() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(1, 0, 0)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.admins[0].user.clone();
+    let token = test_project.admins[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let begin = DateTime::<FixedOffset>::from(Utc::now());
+        let instance_id = random_uuid();
+        let instance_name = random_alphanumeric_string(10);
+        let status = "ACTIVE".to_string();
+        let created = client
+            .server_state
+            .create(
+                begin,
+                instance_id.clone(),
+                instance_name.clone(),
+                flavor.id,
+                status.clone(),
+                user.id,
+            )
+            .send()
+            .unwrap();
+
+        // assert
+        assert_eq!(begin, created.begin);
+        assert_eq!(instance_id, created.instance_id);
+        assert_eq!(instance_name, created.instance_name);
+        assert_eq!(flavor.id, created.flavor);
+        assert_eq!(flavor.name, created.flavor_name);
+        assert_eq!(status, created.status);
+        assert_eq!(user.id, created.user);
+        assert_eq!(user.name, created.username);
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_server_state_create_and_get_works() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(1, 0, 0)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.admins[0].user.clone();
+    let token = test_project.admins[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act and assert 1 - create
+        let begin = DateTime::<FixedOffset>::from(Utc::now());
+        let instance_id = random_uuid();
+        let instance_name = random_alphanumeric_string(10);
+        let status = "ACTIVE".to_string();
+        let created = client
+            .server_state
+            .create(
+                begin,
+                instance_id.clone(),
+                instance_name.clone(),
+                flavor.id,
+                status.clone(),
+                user.id,
+            )
+            .send()
+            .unwrap();
+        assert_eq!(begin, created.begin);
+        assert_eq!(instance_id, created.instance_id);
+        assert_eq!(instance_name, created.instance_name);
+        assert_eq!(flavor.id, created.flavor);
+        assert_eq!(flavor.name, created.flavor_name);
+        assert_eq!(status, created.status);
+        assert_eq!(user.id, created.user);
+        assert_eq!(user.name, created.username);
+
+        // act and assert 2 - get
+        let retrieved = client.server_state.get(created.id).unwrap();
+        assert_eq!(retrieved.id, created.id);
+        assert!((retrieved.begin - created.begin).num_milliseconds() < 1);
+        assert_eq!(retrieved.instance_id, created.instance_id);
+        assert_eq!(retrieved.instance_name, created.instance_name);
+        assert_eq!(retrieved.flavor, created.flavor);
+        assert_eq!(retrieved.flavor_name, created.flavor_name);
+        assert_eq!(retrieved.status, created.status);
+        assert_eq!(retrieved.user, created.user);
+        assert_eq!(retrieved.username, created.username);
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_server_state_create_and_list_works() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(1, 0, 0)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.admins[0].user.clone();
+    let token = test_project.admins[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act and assert 1 - create
+        let begin = DateTime::<FixedOffset>::from(Utc::now());
+        let instance_id = random_uuid();
+        let instance_name = random_alphanumeric_string(10);
+        let status = "ACTIVE".to_string();
+        let created = client
+            .server_state
+            .create(
+                begin,
+                instance_id.clone(),
+                instance_name.clone(),
+                flavor.id,
+                status.clone(),
+                user.id,
+            )
+            .send()
+            .unwrap();
+        assert_eq!(begin, created.begin);
+        assert_eq!(instance_id, created.instance_id);
+        assert_eq!(instance_name, created.instance_name);
+        assert_eq!(flavor.id, created.flavor);
+        assert_eq!(flavor.name, created.flavor_name);
+        assert_eq!(status, created.status);
+        assert_eq!(user.id, created.user);
+        assert_eq!(user.name, created.username);
+
+        // act and assert 2 - list
+        let server_states = client.server_state.list().all().send().unwrap();
+        assert_eq!(server_states.len(), 1);
+        let retrieved = &server_states[0];
+        assert_eq!(retrieved.id, created.id);
+        assert!((retrieved.begin - created.begin).num_milliseconds() < 1);
+        assert_eq!(retrieved.instance_id, created.instance_id);
+        assert_eq!(retrieved.instance_name, created.instance_name);
+        assert_eq!(retrieved.flavor, created.flavor);
+        assert_eq!(retrieved.flavor_name, created.flavor_name);
+        assert_eq!(retrieved.status, created.status);
+        assert_eq!(retrieved.user, created.user);
+        assert_eq!(retrieved.username, created.username);
+    })
+    .await
+    .unwrap();
+}
