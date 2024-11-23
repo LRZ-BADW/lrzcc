ab0fba654fec1ccda3a147ae9481778ad02c1e24
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-23 10:24:41 +0100

    test(test/accounting): add server state delete tests
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ test/tests/accounting/server_state/delete.rs
@@ -0,0 +1,146 @@
+use lrzcc::{Api, Token};
+use lrzcc_test::spawn_app;
+use std::str::FromStr;
+use tokio::task::spawn_blocking;
+
+#[tokio::test]
+async fn e2e_lib_server_state_delete_denies_access_to_normal_user() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(0, 0, 1)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.normals[0].user.clone();
+    let token = test_project.normals[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+    let server_state = server
+        .setup_test_server_state(&flavor, &user)
+        .await
+        .expect("Failed to setup test server state");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let delete = client.server_state.delete(server_state.id);
+
+        // assert
+        assert!(delete.is_err());
+        assert_eq!(
+            delete.unwrap_err().to_string(),
+            format!("Admin privileges required")
+        );
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_server_state_delete_denies_access_to_master_user() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(0, 1, 0)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.masters[0].user.clone();
+    let token = test_project.masters[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+    let server_state = server
+        .setup_test_server_state(&flavor, &user)
+        .await
+        .expect("Failed to setup test server state");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act
+        let delete = client.server_state.delete(server_state.id);
+
+        // assert
+        assert!(delete.is_err());
+        assert_eq!(
+            delete.unwrap_err().to_string(),
+            format!("Admin privileges required")
+        );
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_lib_server_state_delete_works() {
+    // arrange
+    let server = spawn_app().await;
+    let test_project = server
+        .setup_test_project(1, 0, 0)
+        .await
+        .expect("Failed to setup test project");
+    let user = test_project.admins[0].user.clone();
+    let token = test_project.admins[0].token.clone();
+    server
+        .mock_keystone_auth(&token, &user.openstack_id, &user.name)
+        .mount(&server.keystone_server)
+        .await;
+    let flavor = server
+        .setup_test_flavor()
+        .await
+        .expect("Failed to setup test flavor");
+    let server_state = server
+        .setup_test_server_state(&flavor, &user)
+        .await
+        .expect("Failed to setup test server state");
+
+    spawn_blocking(move || {
+        // arrange
+        let client = Api::new(
+            format!("{}/api", &server.address),
+            Token::from_str(&token).unwrap(),
+            None,
+            None,
+        )
+        .unwrap();
+
+        // act and assert 1 - delete
+        client.server_state.delete(server_state.id).unwrap();
+
+        // act and assert 2 - get
+        let get = client.server_state.get(server_state.id);
+        assert!(get.is_err());
+        assert_eq!(
+            get.unwrap_err().to_string(),
+            format!("Server state with given ID not found")
+        );
+    })
+    .await
+    .unwrap();
+}
