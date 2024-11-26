813550d5e7d3d15bda34ce514218cac480d2e7d1
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-26 19:18:34 +0100

    feat(api/authorization): add require_*_or_return_not_found functions
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/authorization.rs
+++ api/src/authorization.rs
@@ -1,13 +1,20 @@
-use crate::error::AuthOnlyError;
+use crate::error::{AuthOnlyError, NotFoundOnlyError};
 use lrzcc_wire::user::User;
-
-// TODO: maybe we need alternative calls that return a not found error to not leak information
 
 pub fn require_admin_user(user: &User) -> Result<(), AuthOnlyError> {
     if !user.is_staff {
         return Err(AuthOnlyError::AuthorizationError(
             "Admin privileges required".to_string(),
         ));
+    }
+    Ok(())
+}
+
+pub fn require_admin_user_or_return_not_found(
+    user: &User,
+) -> Result<(), NotFoundOnlyError> {
+    if !user.is_staff {
+        return Err(NotFoundOnlyError::NotFoundError);
     }
     Ok(())
 }
@@ -25,15 +32,34 @@ pub fn require_master_user(
     Ok(())
 }
 
+pub fn require_master_user_or_return_not_found(
+    user: &User,
+    project_id: u32,
+) -> Result<(), NotFoundOnlyError> {
+    if !user.is_staff && (user.role != 2 || user.project != project_id) {
+        return Err(NotFoundOnlyError::NotFoundError);
+    }
+    Ok(())
+}
+
 pub fn require_project_user(
     user: &User,
     project_id: u32,
 ) -> Result<(), AuthOnlyError> {
     if !user.is_staff && user.project != project_id {
         return Err(AuthOnlyError::AuthorizationError(
-            "Admin or master user privileges for respective project required"
-                .to_string(),
+            "Must be admin or user of respective project".to_string(),
         ));
+    }
+    Ok(())
+}
+
+pub fn require_project_user_or_return_not_found(
+    user: &User,
+    project_id: u32,
+) -> Result<(), NotFoundOnlyError> {
+    if !user.is_staff && user.project != project_id {
+        return Err(NotFoundOnlyError::NotFoundError);
     }
     Ok(())
 }
