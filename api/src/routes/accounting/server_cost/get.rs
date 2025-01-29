210aeb98b9e7eafbd6ff8b009f7ee037ec7bc476
Sandro-Alessio Gierens <sandro@gierens.de>
2025-01-29 10:33:01 +0100

    feat(api/accounting/server_cost): implement calculate_server_cost_for_user_normal
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/accounting/server_cost/get.rs
+++ api/src/routes/accounting/server_cost/get.rs
@@ -2,8 +2,12 @@ use crate::authorization::require_admin_user;
 use crate::database::accounting::server_state::select_user_class_by_server_from_db;
 use crate::database::pricing::flavor_price::select_flavor_prices_for_period_from_db;
 use crate::database::resources::flavor::select_all_flavors_from_db;
+use crate::database::user::user::select_user_class_by_user_from_db;
 use crate::error::{OptionApiError, UnexpectedOnlyError};
-use crate::routes::accounting::server_consumption::get::calculate_server_consumption_for_server;
+use crate::routes::accounting::server_consumption::get::{
+    calculate_server_consumption_for_server,
+    calculate_server_consumption_for_user, ServerConsumptionForUser,
+};
 use actix_web::web::{Data, Query, ReqData};
 use actix_web::HttpResponse;
 use anyhow::{anyhow, Context};
@@ -323,6 +327,70 @@ pub enum ServerCostForUser {
     Detail(ServerCostUser),
 }
 
+pub async fn calculate_server_cost_for_user_normal(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+    begin: DateTime<Utc>,
+    end: DateTime<Utc>,
+) -> Result<ServerCostSimple, UnexpectedOnlyError> {
+    let mut cost = ServerCostSimple { total: 0.0 };
+    let user_class =
+        match select_user_class_by_user_from_db(transaction, user_id)
+            .await?
+            .map(UserClass::from_u32)
+            .map_or(Ok(None), |r| r.map(Some))?
+        {
+            Some(user_class) => user_class,
+            None => return Ok(cost),
+        };
+    let price_periods =
+        get_flavor_price_periods(transaction, begin, end).await?;
+
+    let mut end_times =
+        price_periods.keys().skip(1).cloned().collect::<Vec<_>>();
+    end_times.push(end);
+
+    for ((start_time, prices), end_time) in price_periods.iter().zip(end_times)
+    {
+        let ServerConsumptionForUser::Normal(consumption) =
+            calculate_server_consumption_for_user(
+                transaction,
+                user_id,
+                Some(*start_time),
+                Some(end_time),
+                None,
+            )
+            .await?
+        else {
+            return Err(
+                anyhow!("Unexpected ServerConsumptionForUser variant").into()
+            );
+        };
+        for (flavor_name, flavor_consumption) in consumption {
+            if flavor_consumption > 0. {
+                cost.total += calculate_flavor_consumption_cost(
+                    flavor_consumption,
+                    prices.clone(),
+                    user_class.clone(),
+                    flavor_name,
+                );
+            }
+        }
+    }
+
+    Ok(cost)
+}
+
+// TODO: can we use macros to get rid of the code duplication here
+pub async fn calculate_server_cost_for_user_detail(
+    transaction: &mut Transaction<'_, MySql>,
+    user_id: u64,
+    begin: DateTime<Utc>,
+    end: DateTime<Utc>,
+) -> Result<ServerCostUser, UnexpectedOnlyError> {
+    todo!()
+}
+
 pub async fn calculate_server_cost_for_user(
     transaction: &mut Transaction<'_, MySql>,
     user_id: u64,
@@ -330,7 +398,26 @@ pub async fn calculate_server_cost_for_user(
     end: DateTime<Utc>,
     detail: Option<bool>,
 ) -> Result<ServerCostForUser, UnexpectedOnlyError> {
-    todo!()
+    Ok(match detail {
+        Some(true) => ServerCostForUser::Detail(
+            calculate_server_cost_for_user_detail(
+                transaction,
+                user_id,
+                begin,
+                end,
+            )
+            .await?,
+        ),
+        _ => ServerCostForUser::Normal(
+            calculate_server_cost_for_user_normal(
+                transaction,
+                user_id,
+                begin,
+                end,
+            )
+            .await?,
+        ),
+    })
 }
 
 #[derive(Serialize)]
