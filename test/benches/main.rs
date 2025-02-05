c616d8b2157d1852c7d3c9341a55aa4c49dd78ac
Sandro-Alessio Gierens <sandro@gierens.de>
2025-02-05 14:02:02 +0100

    feat(test): add rudimentary bench
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ test/benches/main.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate bencher;
+
+use bencher::Bencher;
+
+use std::env;
+use std::str::FromStr;
+
+use lrzcc::{Api, Token};
+
+fn bench_hello_user(b: &mut Bencher) {
+    let token =
+        Token::from_str(env::var("OS_TOKEN").unwrap().as_str()).unwrap();
+    let api =
+        Api::new("http://localhost:8000/api".to_string(), token, None, None)
+            .unwrap();
+
+    b.iter(|| {
+        api.hello.user().unwrap();
+    });
+}
+
+benchmark_group!(benches, bench_hello_user);
+benchmark_main!(benches);
