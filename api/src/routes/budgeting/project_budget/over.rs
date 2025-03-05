9d05c0f5b86c8a5c4d4865ad436ebabd987bc7c8
Sandro-Alessio Gierens <sandro@gierens.de>
2025-03-05 10:16:09 +0100

    feat(api/routes/budgeting): implement calculate_project_budget_over_for_budget_normal
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/budgeting/project_budget/over.rs
+++ api/src/routes/budgeting/project_budget/over.rs
@@ -1,9 +1,13 @@
 use crate::authorization::require_admin_user;
+use crate::database::budgeting::project_budget::select_maybe_project_budget_from_db;
 use crate::error::{OptionApiError, UnexpectedOnlyError};
+use crate::routes::accounting::server_cost::get::{
+    calculate_server_cost_for_project, ServerCostForProject,
+};
 use actix_web::web::{Data, Query, ReqData};
 use actix_web::HttpResponse;
-use anyhow::Context;
-use chrono::{DateTime, Utc};
+use anyhow::{anyhow, Context};
+use chrono::{DateTime, Datelike, TimeZone, Utc};
 use lrzcc_wire::budgeting::{
     ProjectBudgetOverDetail, ProjectBudgetOverParams, ProjectBudgetOverSimple,
 };
@@ -17,12 +21,44 @@ pub enum ProjectBudgetOver {
     Normal(Vec<ProjectBudgetOverSimple>),
     Detail(Vec<ProjectBudgetOverDetail>),
 }
+
 pub async fn calculate_project_budget_over_for_budget_normal(
-    _transaction: &mut Transaction<'_, MySql>,
-    _budget_id: u64,
-    _end: DateTime<Utc>,
+    transaction: &mut Transaction<'_, MySql>,
+    // TODO: should be a u32
+    budget_id: u64,
+    end: DateTime<Utc>,
 ) -> Result<Vec<ProjectBudgetOverSimple>, UnexpectedOnlyError> {
-    todo!();
+    let mut overs = vec![];
+    let Some(budget) =
+        select_maybe_project_budget_from_db(transaction, budget_id).await?
+    else {
+        return Ok(overs);
+    };
+    let year = budget.year;
+    if year != end.year() as u32 {
+        return Ok(overs);
+    }
+    // TODO: outsource into function
+    let begin = Utc.with_ymd_and_hms(year as i32, 1, 1, 1, 0, 0).unwrap();
+    let ServerCostForProject::Normal(cost) = calculate_server_cost_for_project(
+        transaction,
+        budget.project as u64,
+        begin,
+        end,
+        None,
+    )
+    .await?
+    else {
+        return Err(anyhow!("Unexpected ServerCostForProject variant.").into());
+    };
+    let over = ProjectBudgetOverSimple {
+        budget_id: budget_id as u32,
+        project_id: budget.project,
+        project_name: budget.project_name,
+        over: cost.total >= budget.amount as f64,
+    };
+    overs.push(over);
+    Ok(overs)
 }
 
 pub async fn calculate_project_budget_over_for_budget_detail(
