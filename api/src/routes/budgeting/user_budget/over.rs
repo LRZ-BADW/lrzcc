a8199f55ca4acd30d75a0af28cb5f201f7358036
Sandro-Alessio Gierens <sandro@gierens.de>
2025-03-12 18:25:15 +0100

    feat(api/routes): implement calculate_user_budget_over_for_all_normal
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/routes/budgeting/user_budget/over.rs
+++ api/src/routes/budgeting/user_budget/over.rs
@@ -3,6 +3,7 @@ use crate::database::budgeting::user_budget::{
     select_maybe_user_budget_by_user_and_year_from_db,
     select_maybe_user_budget_from_db,
     select_user_budgets_by_project_and_year_from_db,
+    select_user_budgets_by_year_from_db,
 };
 use crate::error::{OptionApiError, UnexpectedOnlyError};
 use crate::routes::accounting::server_cost::get::{
@@ -449,10 +450,38 @@ pub async fn calculate_user_budget_over_for_project(
 }
 
 pub async fn calculate_user_budget_over_for_all_normal(
-    _transaction: &mut Transaction<'_, MySql>,
-    _end: DateTime<Utc>,
+    transaction: &mut Transaction<'_, MySql>,
+    end: DateTime<Utc>,
 ) -> Result<Vec<UserBudgetOverSimple>, UnexpectedOnlyError> {
-    todo!()
+    let mut overs = vec![];
+    let year = end.year() as u32;
+    let budgets =
+        select_user_budgets_by_year_from_db(transaction, year).await?;
+    // TODO: outsource into function
+    let begin = Utc.with_ymd_and_hms(year as i32, 1, 1, 1, 0, 0).unwrap();
+    for budget in budgets {
+        let ServerCostForUser::Normal(cost) = calculate_server_cost_for_user(
+            transaction,
+            budget.user as u64,
+            begin,
+            end,
+            None,
+        )
+        .await?
+        else {
+            return Err(
+                anyhow!("Unexpected ServerCostForProject variant.").into()
+            );
+        };
+        let over = UserBudgetOverSimple {
+            budget_id: budget.id,
+            user_id: budget.user,
+            user_name: budget.username,
+            over: cost.total >= budget.amount as f64,
+        };
+        overs.push(over);
+    }
+    Ok(overs)
 }
 
 pub async fn calculate_user_budget_over_for_all_combined(
