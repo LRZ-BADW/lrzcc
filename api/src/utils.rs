f1e83d4c1a7c2cdcf8b14c32d92df53afbd4e645
Sandro-Alessio Gierens <sandro@gierens.de>
2025-06-05 19:13:01 +0200

    refactor(api): outsource utils::start_of_the_year function
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- api/src/utils.rs
+++ api/src/utils.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, TimeZone, Utc};
+
 pub fn e400<T>(e: T) -> actix_web::Error
 where
     T: std::fmt::Debug + std::fmt::Display + 'static,
@@ -10,4 +12,9 @@ where
     T: std::fmt::Debug + std::fmt::Display + 'static,
 {
     actix_web::error::ErrorInternalServerError(e)
+}
+
+pub fn start_of_the_year(year: u32) -> DateTime<Utc> {
+    // TODO: handle this unwrap
+    Utc.with_ymd_and_hms(year as i32, 1, 1, 1, 0, 0).unwrap()
 }
