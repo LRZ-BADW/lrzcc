ee6a5a99ae912c2d25e9b127552d78732fb80eda
Sandro-Alessio Gierens <sandro@gierens.de>
2025-06-27 00:14:17 +0200

    style(wire/error): use inline form args in string
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wire/src/error.rs
+++ wire/src/error.rs
@@ -10,10 +10,10 @@ pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
-    writeln!(f, "{}", e)?;
+    writeln!(f, "{e}")?;
     let mut current = e.source();
     while let Some(cause) = current {
-        writeln!(f, "caused by: {}", cause)?;
+        writeln!(f, "caused by: {cause}")?;
         current = cause.source();
     }
     Ok(())
