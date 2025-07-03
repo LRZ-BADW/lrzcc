ea66815939cd4213eef881e8852fc305f54803b7
Sandro-Alessio Gierens <sandro@gierens.de>
2025-07-03 23:52:30 +0200

    feat(wrapper): issue token using auth data from env vars
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- wrapper/app.py
+++ wrapper/app.py
@@ -1,5 +1,9 @@
 from django.urls import re_path
 from django.shortcuts import render as django_render
+import requests
+from os import getenv
+from http import HTTPStatus
+import atexit
 
 DEBUG = True
 SECRET_KEY = "1234"
@@ -12,10 +16,107 @@ TEMPLATES = [
 ]
 
 
+token = None
+
+
+def issue_api_token(
+    keystone_url, username, password, user_domain_name, project_name, project_domain_id
+):
+    url = keystone_url + "/auth/tokens/"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "auth": {
+            "identity": {
+                "methods": ["password"],
+                "password": {
+                    "user": {
+                        "name": username,
+                        "domain": {"name": user_domain_name},
+                        "password": password,
+                    }
+                },
+            },
+            "scope": {
+                "project": {"name": project_name, "domain": {"id": project_domain_id}}
+            },
+        }
+    }
+
+    try:
+        resp = requests.post(url, headers=headers, json=data)
+    except:
+        return None
+
+    if resp.status_code != HTTPStatus.CREATED:
+        return None
+
+    return resp.headers["X-Subject-Token"]
+
+
+def revoke_api_token(keystone_url, token):
+    url = keystone_url + "/auth/tokens/"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Auth-Token": token,
+        "X-Subject-Token": token,
+    }
+
+    resp = requests.delete(url, headers=headers)
+
+    if resp.status_code != HTTPStatus.NO_CONTENT:
+        return False
+
+    return True
+
+
+def setup():
+    global token
+
+    keystone_url = getenv("OS_AUTH_URL")
+    username = getenv("OS_USERNAME")
+    password = getenv("OS_PASSWORD")
+    user_domain_name = getenv("OS_USER_DOMAIN_NAME")
+    project_name = getenv("OS_PROJECT_NAME")
+    project_domain_id = getenv("OS_PROJECT_DOMAIN_ID")
+
+    if not (
+        keystone_url
+        and username
+        and password
+        and user_domain_name
+        and project_name
+        and project_domain_id
+    ):
+        print("Some OpenStack auth variable is not set! Source the openrc.sh")
+        exit(1)
+
+    token = issue_api_token(
+        keystone_url,
+        username,
+        password,
+        user_domain_name,
+        project_name,
+        project_domain_id,
+    )
+
+
+def cleanup():
+    keystone_url = getenv("OS_AUTH_URL")
+    revoke_api_token(keystone_url, token)
+
+
+def init():
+    setup()
+    atexit.register(cleanup)
+
+
 def home(request):
-    return django_render(request, "index.html", locals())
+    return django_render(request, "index.html", {"token": token})
 
 
 urlpatterns = [
     re_path(r"^$", home, name="homepage"),
 ]
+
+
+init()
